ea0507f355986c3017811fc0a794ba9e826dbbdf
pepo <pepo.ospina@gmail.com>
2024-02-29 01:49:12 +0100

    wip process and sort

--- app/webapp/src/scios/SciOS.tsx
+++ app/webapp/src/scios/SciOS.tsx
@@ -7,58 +7,112 @@ import { getSparql } from '../functionsCalls/post.requests';
 import { AppHeading, AppLabel } from '../ui-components';
 import { Loading } from '../ui-components/LoadingDiv';
 import { useThemeContext } from '../ui-components/ThemedApp';
-import { URLS_QUERY } from './queries';
+import { TOP_URLS_QUERY, URLS_DATA_QUERIES } from './queries';
+
+interface UrlAuthorData {
+  author: string;
+  keywords: Set<string>;
+}
+type UrlsAuthorData = Map<string, UrlAuthorData>;
 
 type Urls = Map<
   string,
   {
-    authors: Array<{ author: string; keywords: string[] }>;
+    score: number;
+    authorsData?: UrlAuthorData;
   }
 >;
+
+type TopUrlsResult = Array<{
+  uniqueRef: { value: string };
+  url: { value: string };
+}>;
+
+type UrlsDataResult = Array<{
+  keyword: { value: string };
+  name: { value: string };
+  post: { value: string };
+  relation: { value: string };
+  url: { value: string };
+}>;
 
 export const AppSciOS = (props: {}) => {
   const { constants } = useThemeContext();
   const { appAccessToken } = useAccountContext();
 
-  const { data: urlsResult } = useQuery({
-    queryKey: ['sparql-urls', appAccessToken],
+  const { data: topUrlsResult } = useQuery<TopUrlsResult>({
+    queryKey: ['sparql-top-urls', appAccessToken],
     queryFn: async () => {
       if (appAccessToken) {
-        return getSparql(URLS_QUERY, appAccessToken);
+        return getSparql(TOP_URLS_QUERY, appAccessToken);
       }
       return null;
     },
   });
 
+  const { data: urlsDataResult } = useQuery<UrlsDataResult>({
+    queryKey: ['sparql-urls-data', appAccessToken],
+    queryFn: async () => {
+      if (appAccessToken) {
+        return getSparql(URLS_DATA_QUERIES, appAccessToken);
+      }
+      return null;
+    },
+  });
+
+  // For each url, store the authors and the keywords of each author
+  const urlsAuthorData = useMemo<UrlsAuthorData | undefined>(() => {
+    const map: UrlsAuthorData = new Map();
+
+    if (urlsDataResult) {
+      urlsDataResult.forEach((urlData) => {
+        const url = urlData.url.value;
+        const val = map.get(url) || {
+          author: urlData.name.value,
+          keywords: new Set(),
+        };
+        val.keywords.add(urlData.keyword.value);
+        map.set(url, val);
+      });
+      return map;
+    }
+  }, [urlsDataResult]);
+
   const topUrls = useMemo<Urls | undefined>(() => {
     const map: Urls = new Map();
-    console.log({ urlsResult });
 
-    map.set('https://google.com', {
-      authors: [
-        {
-          author: 'pepo',
-          keywords: ['one', 'two'],
-        },
-      ],
-    });
+    if (topUrlsResult && urlsAuthorData) {
+      console.log({ topUrlsResult, urlsAuthorData });
 
-    map.set('https://altavista.com', {
-      authors: [
-        {
-          author: 'shahar',
-          keywords: ['one', 'two'],
-        },
-      ],
-    });
+      topUrlsResult.forEach((urlRes) => {
+        // get data of each url
+        const authorsData = urlsAuthorData.get(urlRes.url.value);
+
+        map.set(urlRes.url.value, {
+          score: Number(urlRes.uniqueRef),
+          authorsData: authorsData,
+        });
+      });
+    }
+
     return map;
-  }, [urlsResult]);
+  }, [topUrlsResult, urlsAuthorData]);
+
+  const topUrlsArray = useMemo(() => {
+    if (topUrls) {
+      const unordered = Array.from(topUrls.entries());
+      const ordered = unordered.sort((a, b) =>
+        a[1].score > b[1].score ? 1 : -1
+      );
+      return ordered;
+    }
+  }, [topUrls]);
 
   return (
     <Box pad={{ vertical: 'large' }} align="start" gap="medium">
       <AppHeading level="1">SciOS 2024 - Demo</AppHeading>
-      {topUrls ? (
-        Array.from(topUrls.entries()).map(([url, data]) => {
+      {topUrlsArray ? (
+        topUrlsArray.map(([url, data]) => {
           return (
             <Box
               gap="small"
@@ -67,19 +121,22 @@ export const AppSciOS = (props: {}) => {
                 borderLeft: '4px solid',
                 borderColor: constants.colors.backgroundLightDarker,
               }}>
+              <Text>Score: {data.score}</Text>
               <Anchor href={url}>{url}</Anchor>
-              {data.authors.map((authorData) => {
-                return (
-                  <Box direction="row" align="center" gap="medium">
-                    <Text>{authorData.author}</Text>
-                    <Box direction="row" gap="small">
-                      {authorData.keywords.map((keyword) => {
+              {data.authorsData ? (
+                <Box direction="row" align="center" gap="medium">
+                  <Text>{data.authorsData.author}</Text>
+                  <Box direction="row" gap="small">
+                    {Array.from(data.authorsData.keywords.values()).map(
+                      (keyword) => {
                         return <AppLabel>#{keyword}</AppLabel>;
-                      })}
-                    </Box>
+                      }
+                    )}
                   </Box>
-                );
-              })}
+                </Box>
+              ) : (
+                <></>
+              )}
             </Box>
           );
         })
--- app/webapp/src/scios/queries.ts
+++ app/webapp/src/scios/queries.ts
@@ -1,33 +1,53 @@
-export const URLS_QUERY = `
-  PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-  PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-  PREFIX np: <http://www.nanopub.org/nschema#>
-  PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-  PREFIX dct: <http://purl.org/dc/terms/>
+export const TOP_URLS_QUERY = `
+PREFIX rel: <https://w3id.org/np/RA4-9EhvPmX6Y2w_Ih3BiAZxyRehqm6ZVFW2DYA7v1Rjg#>
+PREFIX dct: <http://purl.org/dc/terms/>
+PREFIX ns1: <https://sense-nets.xyz/>
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX np: <http://www.nanopub.org/nschema#>
 
-  SELECT DISTINCT ?sub ?name ?date WHERE {
-    ?sub a <https://sense-nets.xyz/SemanticPost> ;
-        dct:creator ?creator ;
-        dct:created ?date .
-    ?creator foaf:name ?name .
+SELECT ?url (COUNT(DISTINCT ?creator) AS ?uniqueRef)
+WHERE {
+  ?np a ns1:SemanticPost ;
+      np:hasAssertion ?assertion .
+  ?assertion dct:creator ?creator .
+  ?assertion ?relation ?url .
+   GRAPH  rel:assertion {
+    ?relation rdfs:label ?label .
   }
-  ORDER BY ?date
-  LIMIT 100
+}
+GROUP BY ?url
+ORDER BY DESC(?uniqueRef)
 `;
 
-export const POSTERS_QUERY = `
-  PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-  PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-  PREFIX np: <http://www.nanopub.org/nschema#>
-  PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-  PREFIX dct: <http://purl.org/dc/terms/>
+export const URLS_DATA_QUERIES = `
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX np: <http://www.nanopub.org/nschema#>
+PREFIX dct: <http://purl.org/dc/terms/>
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+PREFIX schema: <https://schema.org/>
+PREFIX rel: <https://w3id.org/np/RA4-9EhvPmX6Y2w_Ih3BiAZxyRehqm6ZVFW2DYA7v1Rjg#>
 
-  SELECT DISTINCT ?sub ?name ?date WHERE {
-    ?sub a <https://sense-nets.xyz/SemanticPost> ;
-        dct:creator ?creator ;
-        dct:created ?date .
-    ?creator foaf:name ?name .
+SELECT DISTINCT ?url ?post ?name ?relation ?keyword
+WHERE {
+  ?np np:hasAssertion ?assertion .
+  ?np dct:created ?date .
+  FILTER(?date > "2024-02-27"^^xsd:date)  # Replace YYYY-MM-DD with your cutoff date
+
+  ?assertion ?relation ?url .
+  GRAPH rel:assertion {
+    ?relation rdfs:label ?label .
   }
-  ORDER BY ?date
-  LIMIT 100
+  ?assertion dct:creator ?creator ;
+             schema:keywords ?keyword .
+  ?creator foaf:name ?name .
+  
+  OPTIONAL{?assertion rdfs:comment ?post }
+  OPTIONAL{?assertion <https://www.w3.org/2000/01/rdf-schema#comment> ?post}
+  
+  # Assuming dct:issued is used for publication date
+  
+}
+LIMIT 1000
 `;
