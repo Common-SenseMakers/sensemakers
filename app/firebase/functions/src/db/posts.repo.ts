5896ef8922c7fb594fd6502f5de1649d5ce367c5
Pepo <pepo.ospina@gmail.com>
2024-03-14 12:08:48 +0100

    deleted legacy code (add it setup by step)

--- app/firebase/functions/src/db/posts.repo.ts
+++
@@ -1,8 +0,0 @@
-import { AppPost, AppPostStore } from '../@shared/types';
-import { collections } from '../db/db';
-
-export const createPost = async (post: AppPostStore): Promise<AppPost> => {
-  const ref = collections.posts.doc();
-  await ref.create(post);
-  return { id: ref.id, ...post };
-};
