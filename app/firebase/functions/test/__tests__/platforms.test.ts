807be9572b3dcf6c7c46feff81f2a2f8a671c018
Wesley Finck <wesleyfinck@gmail.com>
2024-03-21 17:46:18 -0700

    update tests for getting tweets in a time range

--- app/firebase/functions/test/__tests__/platforms.test.ts
+++ app/firebase/functions/test/__tests__/platforms.test.ts
@@ -6,16 +6,21 @@ import { services } from './test.services';
 
 describe('platforms', () => {
   describe('twitter', () => {
-    it('get latest tweets', async () => {
+    it("get's all tweets in a time range", async () => {
+      services;
       const twitterService = services.users.platforms.get(PLATFORM.Twitter) as
         | TwitterService
         | undefined;
       if (!twitterService) {
         throw new Error('Twitter service not found');
       }
-      const tweets = await twitterService.fetch();
+      const tweets = await twitterService.fetch({
+        user_id: '1753077743816777728',
+        start_time: '2024-02-24T00:00:00Z',
+        end_time: '2024-02-25T00:00:00Z',
+      });
       expect(tweets).to.not.be.undefined;
-      expect(tweets.length).to.be.equal(0);
+      expect(tweets.length).to.be.equal(1);
     });
   });
 });
