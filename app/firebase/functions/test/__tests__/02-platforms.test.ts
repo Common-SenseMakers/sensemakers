a2e26ab80db7619f26fb5050664cb1bcdb2e7f67
Wesley Finck <wesleyfinck@gmail.com>
2024-04-03 16:55:19 -0700

    update platform tests to use authenticated test app users

--- app/firebase/functions/test/__tests__/02-platforms.test.ts
+++ app/firebase/functions/test/__tests__/02-platforms.test.ts
@@ -1,38 +1,40 @@
 import { expect } from 'chai';
-import { TwitterApi } from 'twitter-api-v2';
 
-import { PLATFORM } from '../../src/@shared/types';
+import { AppUser, PLATFORM } from '../../src/@shared/types';
 import { logger } from '../../src/instances/logger';
 import { resetDB } from '../__tests_support__/db';
+import { createTestAppUsers } from '../utils/createTestAppUsers';
 import { services } from './test.services';
 
-const TWITTER_ACCOUNT = 'sensemakergod';
-
-const TEST_TOKENS_MAP = JSON.parse(
-  process.env.TEST_USERS_BEARER_TOKENS as string
-);
-
-describe('platforms', () => {
+describe.only('platforms', () => {
   before(async () => {
     logger.debug('resetting DB');
     await resetDB();
   });
 
   describe('twitter', () => {
+    let appUser: AppUser | undefined;
+    before(async () => {
+      const users = await createTestAppUsers();
+      appUser = users[0];
+    });
     it("get's all tweets in a time range using pagination", async () => {
-      services;
+      if (!appUser) {
+        throw new Error('appUser not created');
+      }
+      const usersDetails = appUser[PLATFORM.Twitter];
+      if (!usersDetails) {
+        throw new Error('Unexpected');
+      }
       const twitterService = services.platforms.get(PLATFORM.Twitter);
-
+      let userDetails = usersDetails[0];
+      if (userDetails.read === undefined) {
+        throw new Error('Unexpected');
+      }
       try {
         const tweets = await twitterService.fetch([
           {
-            userDetails: {
-              user_id: TEST_TOKENS_MAP[TWITTER_ACCOUNT].user_id,
-              signupDate: 0,
-              read: {
-                accessToken: TEST_TOKENS_MAP[TWITTER_ACCOUNT].accessToken,
-              },
-            },
+            userDetails,
             start_time: 1708560000000,
             end_time: 1708646400000,
           },
@@ -43,35 +45,6 @@ describe('platforms', () => {
         console.error('error: ', error);
         throw error;
       }
-    });
-    it('instantiates two user-wide API clients and can make authenticated requests', async () => {
-      const appOnlyTwitterClient = new TwitterApi({
-        clientId: process.env.TWITTER_CLIENT_ID as string,
-        clientSecret: process.env.TWITTER_CLIENT_SECRET as string,
-      });
-      const testUserData = require('./test_user_data.json');
-      let testUserDataWithUpdatedTokens: any[] = [];
-      try {
-        for (const user of testUserData) {
-          const { client: refreshedClient, refreshToken } =
-            await appOnlyTwitterClient.refreshOAuth2Token(user.refresh_token);
-          const me = await refreshedClient.v2.me();
-          const updatedUser = {
-            ...user,
-            refresh_token: refreshToken,
-          };
-          testUserDataWithUpdatedTokens.push(updatedUser);
-          expect(me.data).to.not.be.undefined;
-        }
-      } catch (error) {
-        console.error('error: ', error);
-        throw error;
-      }
-      const fs = require('fs');
-      fs.writeFileSync(
-        './test/__tests__/test_user_data.json',
-        JSON.stringify(testUserDataWithUpdatedTokens)
-      );
     });
   });
 });
