e194a8414e68cf1dd9cb43ad4fc7c68dfb9e63bb
Wesley Finck <wesleyfinck@gmail.com>
2024-04-03 16:53:40 -0700

    add new function for creating test app users from twitter account credentials and use in the post process test

---
+++ app/firebase/functions/test/utils/createTestAppUsers.ts
@@ -0,0 +1,46 @@
+import { AppUser, PLATFORM } from '../../src/@shared/types';
+import { getPrefixedUserId } from '../../src/users/users.utils';
+import {
+  NUM_TWITTER_USERS,
+  testTwitterAccountTokens,
+} from '../__tests__/setup';
+import { services } from '../__tests__/test.services';
+import { TwitterAccountCredentials } from './authenticateTwitterUsers';
+
+const TWITTER_ACCOUNTS: TwitterAccountCredentials[] = JSON.parse(
+  process.env.TEST_USER_TWITTER_ACCOUNTS as string
+);
+
+export const createTestAppUsers = async (): Promise<AppUser[]> => {
+  if (!TWITTER_ACCOUNTS) {
+    throw new Error('test acccounts undefined');
+  }
+  if (TWITTER_ACCOUNTS.length < NUM_TWITTER_USERS) {
+    throw new Error('not enough twitter account credentials provided');
+  }
+  /** store some real twitter users in the DB */
+  const users: AppUser[] = TWITTER_ACCOUNTS.splice(0, NUM_TWITTER_USERS).map(
+    (twitterAccount): AppUser => {
+      const twitter = testTwitterAccountTokens.get(twitterAccount.username);
+      if (!twitter) {
+        throw new Error('Unexpected');
+      }
+      const user_id = twitterAccount.user_id;
+      const userId = getPrefixedUserId(PLATFORM.Twitter, user_id);
+      return {
+        userId,
+        platformIds: [userId],
+        twitter: [twitter],
+      };
+    }
+  );
+
+  await Promise.all(
+    users.map((user) => services.users.repo.createUser(user.userId, user))
+  );
+
+  /** wait for just a second */
+  await new Promise<void>((resolve) => setTimeout(resolve, 1000));
+
+  return users;
+};
