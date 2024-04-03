fbcca2f104db48dd510609f56abbfb861c904d01
Wesley Finck <wesleyfinck@gmail.com>
2024-04-03 12:58:40 -0700

    move authentication flow to function and update tests

--- app/firebase/functions/test/__tests__/04-twitter.integration.test.ts
+++ app/firebase/functions/test/__tests__/04-twitter.integration.test.ts
@@ -1,158 +1,47 @@
 import { expect } from 'chai';
-import puppeteer, { Browser, Page } from 'puppeteer';
-import { IOAuth2RequestTokenResult } from 'twitter-api-v2';
+import puppeteer from 'puppeteer';
+import { TwitterApi } from 'twitter-api-v2';
 
-import { PLATFORM } from '../../src/@shared/types';
-import { TwitterGetContextParams } from '../../src/@shared/types.twitter';
-import { IdentityServicesMap } from '../../src/platforms/platforms.service';
 import { TwitterService } from '../../src/platforms/twitter/twitter.service';
-import { UsersService } from '../../src/users/users.service';
-import { resetDB } from '../__tests_support__/db';
+import { TimeService } from '../../src/time/time.service';
+import {
+  TwitterAccountCredentials,
+  authenticateTwitterUser,
+} from '../utils/authenticateTwitterUsers';
 import { userRepo } from './test.services';
 
-const logger = (global as any).logger;
-
-const TWITTER_ACCOUNT = 'sense_nets_bot';
-const CALLBACK_URL = 'http://127.0.0.1:3000/';
-const NEXT_BUTTON_TEXT = 'Next';
-const LOG_IN_BUTTON_TEXT = 'Log in';
-const AUTHORIZE_APP_BUTTON_TEXT = 'Authorize app';
-const TEST_ACCOUNTS_MAP = JSON.parse(
+const TEST_ACCOUNTS = JSON.parse(
   process.env.TEST_USER_TWITTER_CREDENTIALS as string
 );
 
-describe('twitter integration', () => {
-  const identityServices: IdentityServicesMap = new Map();
-  const twitter = new TwitterService({
-    clientId: process.env.TWITTER_CLIENT_ID as string,
-    clientSecret: process.env.TWITTER_CLIENT_SECRET as string,
-  });
-  identityServices.set(PLATFORM.Twitter, twitter);
-  const usersService = new UsersService(userRepo, identityServices, {
-    tokenSecret: process.env.OUR_TOKEN_SECRET as string,
-    expiresIn: '30d',
-  });
-  let twitterOAuthTokenRequestResult:
-    | (IOAuth2RequestTokenResult & TwitterGetContextParams)
-    | undefined;
-  let twitterOAuthCode: string | undefined;
-  let browser: Browser | undefined;
-  let page: Page | undefined;
-  let testAccount: {
-    user_id: string;
-    username: string;
-    password: string;
-  } = TEST_ACCOUNTS_MAP[TWITTER_ACCOUNT];
+describe.only('twitter integration', () => {
+  let testAccount: TwitterAccountCredentials = TEST_ACCOUNTS[1];
   if (!testAccount) {
     throw new Error('testAccount undefined');
   }
 
-  before(async () => {
-    logger.debug('resetting DB');
-    await resetDB();
-    browser = await puppeteer.launch({ headless: false });
-    page = await browser.newPage();
+  const time = new TimeService();
+  const twitterService = new TwitterService(time, userRepo, {
+    clientId: process.env.TWITTER_CLIENT_ID as string,
+    clientSecret: process.env.TWITTER_CLIENT_SECRET as string,
   });
 
-  describe('connect twitter', () => {
-    it('get twitter oauth details', async () => {
-      const details = await usersService.getSignupContext(
-        PLATFORM.Twitter,
-        undefined,
-        {
-          callback_url: CALLBACK_URL,
-        }
-      );
+  it('authenticates a twitter user with the oauth 2.0 flow', async () => {
+    const browser = await puppeteer.launch({ headless: false });
+    const userTokens = await authenticateTwitterUser(
+      testAccount,
+      twitterService,
+      browser,
+      'read'
+    );
+    expect(userTokens).to.not.be.undefined;
+    // expect(userTokens.length).to.eq(1);
+    if (!userTokens.read?.accessToken) {
+      throw new Error('unexpected');
+    }
 
-      logger.debug(`details:`, { details });
-      expect(details).to.not.be.undefined;
-
-      expect(details.callback_url).to.not.be.undefined;
-      expect(details.codeChallenge).to.not.be.undefined;
-      expect(details.codeVerifier).to.not.be.undefined;
-      expect(details.state).to.not.be.undefined;
-      expect(details.url.startsWith('https://twitter.com')).to.be.true;
-
-      twitterOAuthTokenRequestResult = details;
-    });
-    it('authenticate twitter account when navigating to the generated oauth link', async () => {
-      if (!twitterOAuthTokenRequestResult || !browser || !page) {
-        throw new Error('unexpected');
-      }
-
-      const usernameInputSelector = 'input[autocomplete="username"]';
-      const passwordInputSelector = 'input[autocomplete="current-password"]';
-
-      await page.goto(twitterOAuthTokenRequestResult.url);
-      await page.waitForSelector(usernameInputSelector);
-      await page.type(usernameInputSelector, testAccount.username);
-      await page.evaluate((nextButtonText) => {
-        const elements = [...document.querySelectorAll('span')];
-        const targetElement = elements.find(
-          (e) => e.innerText == nextButtonText
-        );
-        if (targetElement) targetElement.click();
-      }, NEXT_BUTTON_TEXT);
-
-      await page.waitForSelector(passwordInputSelector);
-      await page.type(passwordInputSelector, testAccount.password);
-      await page.evaluate((logInButtonText) => {
-        const elements = [...document.querySelectorAll('span')];
-        const targetElement = elements.find(
-          (e) => e.innerText == logInButtonText
-        );
-        if (targetElement) targetElement.click();
-      }, LOG_IN_BUTTON_TEXT);
-
-      await page.waitForNavigation();
-      await page.waitForFunction(
-        (authorizeAppButtonText) =>
-          document.body.textContent?.includes(authorizeAppButtonText),
-        {},
-        AUTHORIZE_APP_BUTTON_TEXT
-      );
-      await page.evaluate((authorizeAppButtonText) => {
-        const elements = [...document.querySelectorAll('span')];
-        const targetElement = elements.find(
-          (e) => e.innerText == authorizeAppButtonText
-        );
-        if (targetElement) targetElement.click();
-      }, AUTHORIZE_APP_BUTTON_TEXT);
-
-      await page.waitForNavigation();
-      const currentUrl = page.url();
-
-      const queryParams = new URLSearchParams(new URL(currentUrl).search);
-
-      const code = queryParams.get('code');
-      twitterOAuthCode = code ? code : undefined;
-    });
-
-    it('handle twitter signup after authenticating the account', async () => {
-      const signupResult = await usersService.handleSignup(
-        PLATFORM.Twitter,
-        {
-          ...twitterOAuthTokenRequestResult,
-          code: twitterOAuthCode,
-        },
-        undefined
-      );
-
-      if (!signupResult) {
-        throw new Error('signupResult undefined');
-      }
-      const user = await usersService.repo.getUser(signupResult.userId);
-
-      if (!user) {
-        throw new Error('user undefined');
-      }
-
-      const twitterDetails = user[PLATFORM.Twitter];
-      if (!twitterDetails) {
-        throw new Error('twitterDetails undefined');
-      }
-
-      expect(twitterDetails[0].user_id).to.eq(testAccount.user_id);
-    });
+    const twitterClient = new TwitterApi(userTokens.read?.accessToken);
+    const { data: userObject } = await twitterClient.v2.me();
+    expect(userObject).to.not.be.undefined;
   });
 });
---
+++ app/firebase/functions/test/utils/authenticateTwitterUsers.ts
@@ -0,0 +1,108 @@
+import puppeteer, { Browser } from 'puppeteer';
+import { IOAuth2RequestTokenResult } from 'twitter-api-v2';
+
+import {
+  TwitterGetContextParams,
+  TwitterUserDetails,
+} from '../../src/@shared/types.twitter';
+import { TwitterService } from '../../src/platforms/twitter/twitter.service';
+import { TimeService } from '../../src/time/time.service';
+import { userRepo } from '../__tests__/test.services';
+
+const CALLBACK_URL = 'https://sense-nets.xyz/';
+const NEXT_BUTTON_TEXT = 'Next';
+const LOG_IN_BUTTON_TEXT = 'Log in';
+const AUTHORIZE_APP_BUTTON_TEXT = 'Authorize app';
+const USERNAME_INPUT_SELECTOR = 'input[autocomplete="username"]';
+const PASSWORD_INPUT_SELECTOR = 'input[autocomplete="current-password"]';
+
+export interface TwitterAccountCredentials {
+  username: string;
+  password: string;
+}
+
+export const authenticateTwitterUsers = async (
+  userCredentials: TwitterAccountCredentials[]
+): Promise<TwitterUserDetails[]> => {
+  const time = new TimeService();
+  const twitterService = new TwitterService(time, userRepo, {
+    clientId: process.env.TWITTER_CLIENT_ID as string,
+    clientSecret: process.env.TWITTER_CLIENT_SECRET as string,
+  });
+
+  const browser = await puppeteer.launch({ headless: false });
+
+  const authenticatedUserPromises = userCredentials.map(async (testAccount) => {
+    return authenticateTwitterUser(
+      testAccount,
+      twitterService,
+      browser,
+      'read'
+    );
+  });
+  browser.close();
+  return Promise.all(authenticatedUserPromises);
+};
+
+export const authenticateTwitterUser = async (
+  user: TwitterAccountCredentials,
+  twitterService: TwitterService,
+  browser: Browser,
+  type: 'read' | 'write'
+): Promise<TwitterUserDetails> => {
+  const twitterOAuthTokenRequestResult: IOAuth2RequestTokenResult &
+    TwitterGetContextParams = await twitterService.getSignupContext(undefined, {
+    callback_url: CALLBACK_URL,
+    type,
+  });
+  const page = await browser.newPage();
+  await page.goto(twitterOAuthTokenRequestResult.url);
+  await page.waitForSelector(USERNAME_INPUT_SELECTOR);
+  await page.type(USERNAME_INPUT_SELECTOR, user.username);
+  await page.evaluate((nextButtonText) => {
+    const elements = [...document.querySelectorAll('span')];
+    const targetElement = elements.find((e) => e.innerText == nextButtonText);
+    if (targetElement) targetElement.click();
+  }, NEXT_BUTTON_TEXT);
+
+  await page.waitForSelector(PASSWORD_INPUT_SELECTOR);
+  await page.type(PASSWORD_INPUT_SELECTOR, user.password);
+  await page.evaluate((logInButtonText) => {
+    const elements = [...document.querySelectorAll('span')];
+    const targetElement = elements.find((e) => e.innerText == logInButtonText);
+    if (targetElement) targetElement.click();
+  }, LOG_IN_BUTTON_TEXT);
+
+  await page.waitForNavigation();
+  await page.waitForFunction(
+    (authorizeAppButtonText) =>
+      document.body.textContent?.includes(authorizeAppButtonText),
+    {},
+    AUTHORIZE_APP_BUTTON_TEXT
+  );
+  await page.evaluate((authorizeAppButtonText) => {
+    const elements = [...document.querySelectorAll('span')];
+    const targetElement = elements.find(
+      (e) => e.innerText == authorizeAppButtonText
+    );
+    if (targetElement) targetElement.click();
+  }, AUTHORIZE_APP_BUTTON_TEXT);
+
+  await page.waitForNavigation();
+  const currentUrl = page.url();
+  page.close();
+
+  const queryParams = new URLSearchParams(new URL(currentUrl).search);
+
+  const twitterOAuthCode = queryParams.get('code');
+  if (!twitterOAuthCode) {
+    throw new Error('twitterOAuthCode undefined');
+  }
+
+  const userDetails = await twitterService.handleSignupData({
+    ...twitterOAuthTokenRequestResult,
+    code: twitterOAuthCode,
+  });
+
+  return userDetails;
+};
