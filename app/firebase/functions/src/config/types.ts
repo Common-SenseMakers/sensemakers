5634ed9775bb1bbdb5efc1e3b5d2ba8394594254
Pepo <pepo.ospina@gmail.com>
2024-04-30 19:06:31 +0200

    updated enpoint

--- app/firebase/functions/src/config/types.ts
+++ app/firebase/functions/src/config/types.ts
@@ -7,7 +7,4 @@ export interface Env {
   TOKEN_SECRET: string;
   TWITTER_API_KEY: string;
   TWITTER_API_SECRET_KEY: string;
-  TWITTER_CLIENT_ID: string;
-  TWITTER_CLIENT_SECRET: string;
-  TWITTER_BEARER_TOKEN: string;
 }
