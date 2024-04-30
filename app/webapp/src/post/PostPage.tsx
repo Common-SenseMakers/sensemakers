be133e68960f3fede5a5093c7a07bb763b0f5a35
Pepo <pepo.ospina@gmail.com>
2024-04-30 10:09:27 +0200

    move posts-list-aware logic outside of postView

--- app/webapp/src/post/PostPage.tsx
+++ app/webapp/src/post/PostPage.tsx
@@ -1,16 +1,29 @@
 import { useParams } from 'react-router-dom';
 
-import { UserPostsContext } from '../user-home/UserPostsContext';
+import { UserPostsContext, useUserPosts } from '../user-home/UserPostsContext';
 import { PostContext } from './PostContext';
 import { PostView } from './PostView';
 
 /** extract the postId from the route and pass it to a PostContext */
 export const PostPage = () => {
   const { id } = useParams();
+  const { posts } = useUserPosts();
+
+  const currPostIndex = posts?.findIndex((p) => p.id === id);
+  const prevPostId =
+    posts && currPostIndex && currPostIndex > 0
+      ? posts[currPostIndex - 1].id
+      : undefined;
+
+  const nextPostId =
+    posts && currPostIndex && currPostIndex < posts.length - 1
+      ? posts[currPostIndex + 1].id
+      : undefined;
+
   return (
     <UserPostsContext>
       <PostContext postId={id}>
-        <PostView></PostView>
+        <PostView prevPostId={prevPostId} nextPostId={nextPostId}></PostView>
       </PostContext>
     </UserPostsContext>
   );
