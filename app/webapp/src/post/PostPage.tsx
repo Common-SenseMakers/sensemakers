9f0212265def74fc704bee1ff96fbf7ff817994b
Pepo <pepo.ospina@gmail.com>
2024-05-03 15:33:39 +0200

    reinstatiate postview

--- app/webapp/src/post/PostPage.tsx
+++ app/webapp/src/post/PostPage.tsx
@@ -1,12 +1,8 @@
-import { Box } from 'grommet';
 import { useParams } from 'react-router-dom';
 
-import { AppBottomNav } from '../app/layout/AppBottomNav';
-import { ViewportPage } from '../app/layout/Viewport';
 import { useUserPosts } from '../user-home/UserPostsContext';
-import { PostContent } from './PostContent';
 import { PostContext } from './PostContext';
-import { PostHeader } from './PostHeader';
+import { PostView } from './PostView';
 
 /** extract the postId from the route and pass it to a PostContext */
 export const PostPage = () => {
@@ -24,24 +20,9 @@ export const PostPage = () => {
       ? posts[currPostIndex + 1].id
       : undefined;
 
-  const content = (
-    <Box round="small">
-      <PostHeader prevPostId={prevPostId} nextPostId={nextPostId}></PostHeader>
-      <PostContent></PostContent>
-    </Box>
-  );
-
-  const rightClicked = () => {};
-
   return (
-    <ViewportPage
-      content={<PostContext postId={id}>{content}</PostContext>}
-      nav={
-        <AppBottomNav
-          paths={[
-            { action: rightClicked, label: 'ignore' },
-            { action: rightClicked, label: 'approve' },
-          ]}></AppBottomNav>
-      }></ViewportPage>
+    <PostContext postId={id}>
+      <PostView prevPostId={prevPostId} nextPostId={nextPostId}></PostView>
+    </PostContext>
   );
 };
---
+++ app/webapp/src/post/PostView.tsx
@@ -0,0 +1,85 @@
+import { Box } from 'grommet';
+import { connect } from 'http2';
+
+import { useAppFetch } from '../api/app.fetch';
+import { AppBottomNav } from '../app/layout/AppBottomNav';
+import { ViewportPage } from '../app/layout/Viewport';
+import { PLATFORM } from '../shared/types/types';
+import { AppPostFull } from '../shared/types/types.posts';
+import { useAccountContext } from '../user-login/contexts/AccountContext';
+import { useNanopubContext } from '../user-login/contexts/platforms/nanopubs/NanopubContext';
+import { PostContent } from './PostContent';
+import { usePost } from './PostContext';
+import { PostHeader } from './PostHeader';
+
+/** extract the postId from the route and pass it to a PostContext */
+export const PostView = (props: {
+  prevPostId?: string;
+  nextPostId?: string;
+}) => {
+  const { prevPostId, nextPostId } = props;
+  const { post, nanopubDraft, nanopubPublished } = usePost();
+  const { connectedUser } = useAccountContext();
+  const { signNanopublication, connect } = useNanopubContext();
+  const appFetch = useAppFetch();
+
+  const approve = async () => {
+    // mark nanopub draft as approved
+    const nanopub = post?.mirrors.find(
+      (m) => m.platformId === PLATFORM.Nanopub
+    );
+
+    if (!nanopub || !nanopub.draft) {
+      throw new Error(`Unexpected nanopub mirror not found`);
+    }
+
+    if (signNanopublication) {
+      const signed = await signNanopublication(nanopub.draft.post);
+      nanopub.draft.postApproval = 'approved';
+      nanopub.draft.post = signed.rdf();
+
+      await appFetch<void, AppPostFull>('/api/posts/approve', post);
+    }
+  };
+
+  const canPublishNanopub =
+    connectedUser &&
+    connectedUser.nanopub &&
+    connectedUser.nanopub.length > 0 &&
+    signNanopublication &&
+    nanopubDraft &&
+    !nanopubPublished;
+
+  const { action: rightClicked, label: rightLabel } = (() => {
+    if (!canPublishNanopub) {
+      return {
+        action: () => connect(),
+        label: 'connect wallet',
+      };
+    } else {
+      return {
+        action: () => approve(),
+        label: 'approve',
+      };
+    }
+  })();
+
+  return (
+    <ViewportPage
+      content={
+        <Box round="small">
+          <PostHeader
+            prevPostId={prevPostId}
+            nextPostId={nextPostId}></PostHeader>
+          <PostContent></PostContent>
+        </Box>
+      }
+      nav={
+        <AppBottomNav
+          paths={[
+            { action: rightClicked, label: 'ignore' },
+            { action: rightClicked, label: rightLabel },
+          ]}></AppBottomNav>
+      }></ViewportPage>
+  );
+};
