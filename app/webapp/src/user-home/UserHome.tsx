24153d5a06d863c1d3b6ee488aa370d7fcfd0c9a
Wesley Finck <wesleyfinck@gmail.com>
2024-05-06 22:15:45 -0700

    use menu to filter posts

--- app/webapp/src/user-home/UserHome.tsx
+++ app/webapp/src/user-home/UserHome.tsx
@@ -1,15 +1,19 @@
-import { Box } from 'grommet';
+import { Box, Menu } from 'grommet';
 import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { useToastContext } from '../app/ToastsContext';
 import { PostCard } from '../post/PostCard';
 import { PostContext } from '../post/PostContext';
+import { AbsoluteRoutes } from '../route.names';
+import { PostsQueryStatusParam } from '../shared/types/types.posts';
 import { BoxCentered } from '../ui-components/BoxCentered';
 import { useUserPosts } from './UserPostsContext';
 
 export const UserHome = () => {
   const { show } = useToastContext();
   const { posts, isLoading, error } = useUserPosts();
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (error) {
@@ -32,18 +36,53 @@ export const UserHome = () => {
   }
 
   return (
-    <Box
-      fill
-      gap="large"
-      pad={{ vertical: 'large', horizontal: 'medium' }}
-      justify="start">
-      {posts.map((post, ix) => (
-        <Box key={ix}>
-          <PostContext postInit={post}>
-            <PostCard></PostCard>
-          </PostContext>
-        </Box>
-      ))}
-    </Box>
+    <>
+      <Menu
+        label="Menu"
+        items={[
+          {
+            label: 'All',
+            onClick: () =>
+              navigate(
+                `${AbsoluteRoutes.Posts}?status=${PostsQueryStatusParam.ALL}`
+              ),
+          },
+          {
+            label: 'For Review',
+            onClick: () =>
+              navigate(
+                `${AbsoluteRoutes.Posts}?status=${PostsQueryStatusParam.FOR_REVIEW}`
+              ),
+          },
+          {
+            label: 'Ignored',
+            onClick: () =>
+              navigate(
+                `${AbsoluteRoutes.Posts}?status=${PostsQueryStatusParam.IGNORED}`
+              ),
+          },
+          {
+            label: 'Published',
+            onClick: () =>
+              navigate(
+                `${AbsoluteRoutes.Posts}?status=${PostsQueryStatusParam.PUBLISHED}`
+              ),
+          },
+        ]}
+      />
+      <Box
+        fill
+        gap="large"
+        pad={{ vertical: 'large', horizontal: 'medium' }}
+        justify="start">
+        {posts.map((post, ix) => (
+          <Box key={ix}>
+            <PostContext postInit={post}>
+              <PostCard></PostCard>
+            </PostContext>
+          </Box>
+        ))}
+      </Box>
+    </>
   );
 };
