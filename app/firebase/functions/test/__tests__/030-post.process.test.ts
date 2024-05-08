848857b176099b9b4001294562ddfbfcf9d3cdd7
Pepo <pepo.ospina@gmail.com>
2024-05-08 10:49:32 +0200

    cleaned

--- app/firebase/functions/test/__tests__/030-post.process.test.ts
+++ app/firebase/functions/test/__tests__/030-post.process.test.ts
@@ -31,7 +31,7 @@ import { createTestAppUsers } from '../utils/user.factory';
 import { USE_REAL_NANOPUB, USE_REAL_PARSER, USE_REAL_TWITTER } from './setup';
 import { getTestServices } from './test.services';
 
-describe('03-process', () => {
+describe('030-process', () => {
   let rsaKeys: RSAKeys | undefined;
   const services = getTestServices({
     twitter: USE_REAL_TWITTER ? 'real' : 'mock-publish',
@@ -224,139 +224,6 @@ describe('03-process', () => {
 
       expect(approved).to.not.be.undefined;
       expect(approved.reviewedStatus).to.equal('reviewed');
-    });
-  });
-  describe.only('query and filter', async () => {
-    before(async () => {
-      const { posts, platformPosts } = services.postsManager.processing;
-      const appPostCreates: [AppPostCreate, PlatformPostCreate][] = [
-        /** Published posts */
-        [
-          {
-            content: 'test content 1',
-            authorId: 'test-user-id',
-            origin: PLATFORM.Nanopub,
-            createdAtMs: 12345678,
-            parsingStatus: AppPostParsingStatus.IDLE,
-            parsedStatus: AppPostParsedStatus.PROCESSED,
-            reviewedStatus: AppPostReviewStatus.APPROVED,
-            republishedStatus: AppPostRepublishedStatus.REPUBLISHED,
-            semantics: 'semantics',
-            mirrorsIds: [],
-          },
-          {
-            platformId: PLATFORM.Nanopub,
-            publishStatus: PlatformPostPublishStatus.PUBLISHED,
-            publishOrigin: PlatformPostPublishOrigin.POSTED,
-          },
-        ],
-        /** Ignored posts */
-        [
-          {
-            content: 'test content 2',
-            authorId: 'test-user-id',
-            origin: PLATFORM.Nanopub,
-            createdAtMs: 12345678,
-            parsingStatus: AppPostParsingStatus.IDLE,
-            parsedStatus: AppPostParsedStatus.PROCESSED,
-            reviewedStatus: AppPostReviewStatus.APPROVED,
-            republishedStatus: AppPostRepublishedStatus.REPUBLISHED,
-            semantics: 'semantics',
-            mirrorsIds: [],
-          },
-          {
-            platformId: PLATFORM.Nanopub,
-            publishStatus: PlatformPostPublishStatus.DRAFT,
-            publishOrigin: PlatformPostPublishOrigin.POSTED,
-          },
-        ],
-        [
-          {
-            content: 'test content 3',
-            authorId: 'test-user-id',
-            origin: PLATFORM.Nanopub,
-            createdAtMs: 12345678,
-            parsingStatus: AppPostParsingStatus.IDLE,
-            parsedStatus: AppPostParsedStatus.PROCESSED,
-            reviewedStatus: AppPostReviewStatus.PENDING,
-            republishedStatus: AppPostRepublishedStatus.REPUBLISHED,
-            originalParsed: {
-              filter_clasification: SciFilterClassfication.NOT_RESEARCH,
-              semantics: 'semantics',
-            },
-            semantics: 'semantics',
-            mirrorsIds: [],
-          },
-          {
-            platformId: PLATFORM.Nanopub,
-            publishStatus: PlatformPostPublishStatus.DRAFT,
-            publishOrigin: PlatformPostPublishOrigin.POSTED,
-          },
-        ],
-        /** For review posts */
-        [
-          {
-            content: 'test content 4',
-            authorId: 'test-user-id',
-            origin: PLATFORM.Nanopub,
-            createdAtMs: 12345678,
-            parsingStatus: AppPostParsingStatus.IDLE,
-            parsedStatus: AppPostParsedStatus.PROCESSED,
-            reviewedStatus: AppPostReviewStatus.PENDING,
-            republishedStatus: AppPostRepublishedStatus.REPUBLISHED,
-            originalParsed: {
-              filter_clasification: SciFilterClassfication.RESEARCH,
-              semantics: 'semantics',
-            },
-            semantics: 'semantics',
-            mirrorsIds: [],
-          },
-          {
-            platformId: PLATFORM.Nanopub,
-            publishStatus: PlatformPostPublishStatus.DRAFT,
-            publishOrigin: PlatformPostPublishOrigin.POSTED,
-          },
-        ],
-      ];
-
-      await services.db.run(async (manager) => {
-        appPostCreates.forEach(([appPostCreate, platformPostCreate]) => {
-          const appPost = posts.create(appPostCreate, manager);
-          const platformPost = platformPosts.create(
-            platformPostCreate,
-            manager
-          );
-          posts.addMirror(appPost.id, platformPost.id, manager);
-        });
-      });
-    });
-    it('gets all posts from a user', async () => {
-      const posts = await services.postsManager.getOfUser('test-user-id', {
-        status: PostsQueryStatusParam.ALL,
-        fetchParams: { expectedAmount: 10 },
-      });
-      expect(posts).to.have.length(4);
-    });
-    it('gets all published posts from a user', async () => {
-      const posts = await services.postsManager.getOfUser('test-user-id', {
-        status: PostsQueryStatusParam.PUBLISHED,
-        fetchParams: { expectedAmount: 10 },
-      });
-      expect(posts).to.have.length(1);
-    });
-    it('gets all for review posts from a user', async () => {
-      const posts = await services.postsManager.getOfUser('test-user-id', {
-        status: PostsQueryStatusParam.PENDING,
-        fetchParams: { expectedAmount: 10 },
-      });
-      expect(posts).to.have.length(1);
-    });
-    it('gets all ignored posts from a user', async () => {
-      const posts = await services.postsManager.getOfUser('test-user-id', {
-        status: PostsQueryStatusParam.IGNORED,
-        fetchParams: { expectedAmount: 10 },
-      });
-      expect(posts).to.have.length(2);
     });
   });
 });
