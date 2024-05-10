92f0448130da6ea4d7420f89316259aeadbde577
Ronen Tamari <ronen.tamari1@mail.huji.ac.il>
2024-05-10 12:26:38 -0400

    #81 add test

---
+++ nlp/tests/test_pydantic_parser.py
@@ -0,0 +1,53 @@
+import sys
+from pathlib import Path
+
+ROOT = Path(__file__).parents[1]
+sys.path.append(str(ROOT))
+import os
+import pytest
+from pydantic import ValidationError
+
+from utils import create_multi_chain_for_tests, create_multi_config_for_tests
+from desci_sense.shared_functions.parsers.multi_chain_parser import MultiChainParser
+from desci_sense.shared_functions.schema.ontology_base import OntologyBase
+from desci_sense.shared_functions.configs import (
+    OpenrouterAPIConfig,
+    WandbConfig,
+    LLMConfig,
+    KeywordPParserChainConfig,
+    RefTaggerChainConfig,
+    TopicsPParserChainConfig,
+    validate_env_var,
+    MetadataExtractionConfig,
+    MultiParserChainConfig,
+    MultiRefTaggerChainConfig,
+    ParserChainType,
+    PostProcessType,
+)  # Adjust the import as necessary
+from desci_sense.shared_functions.dataloaders import (
+    scrape_post,
+    convert_text_to_ref_post,
+)
+from desci_sense.shared_functions.utils import _find_json_object
+from desci_sense.shared_functions.parsers.multi_reference_tagger import normalize_labels
+from desci_sense.shared_functions.postprocessing import Answer, SubAnswer
+
+
+def test_parse_1():
+    json_str = """```json
+    {
+    "sub_answers": [
+        {
+        "reasoning_steps": "The post mentions the recommendation of two papers, indicating a positive sentiment towards them.",
+        "candidate_tags": {
+            "<dg-observation>": "The post is articulating a positive observation about the recommended papers.",
+            "<reading>": "The author is likely looking forward to reading the recommended papers in the future.",
+            "<missing-ref>": "There is no explicit link provided to the papers mentioned.",
+        },
+        "final_answer": ["<dg-observation>", "<reading>", "<missing-ref>"]
+        }
+    ]
+    }
+    ```"""
+    parsed = _find_json_object(json_str)
+    assert "[System error]" not in parsed
