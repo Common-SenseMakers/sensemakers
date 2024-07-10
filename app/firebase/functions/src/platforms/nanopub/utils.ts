8d57f59908ae208c888009b7b75f7205f475a5d0
Sha <shahar.r.oriel@gmail.com>
2024-07-10 10:49:53 +0300

    Added update functionality to buildIntroNp

--- app/firebase/functions/src/platforms/nanopub/utils.ts
+++ app/firebase/functions/src/platforms/nanopub/utils.ts
@@ -449,7 +449,7 @@ export const buildIntroNp = async (
   options: BuildIntroNpOptions = {}
 ): Promise<Nanopub> => {
   return new Promise((resolve, reject) => {
-    const { orcidId, signDelegation } = options;
+    const { orcidId, signDelegation, oldNpUri } = options;
     const headStore = buildNpHead();
 
     // Define the graph URIs
@@ -630,6 +630,14 @@ export const buildIntroNp = async (
       twitterNode,
       pubinfoGraph
     );
+    if (oldNpUri) {
+        writer.addQuad(
+          namedNode('http://purl.org/nanopub/temp/mynanopub#'),
+          namedNode('http://purl.org/nanopub/x/supersesdes'),
+          namedNode(oldNpUri),
+          pubinfoGraph
+        );
+      }
 
     // Instantiate Nanopub profile (ORCID and name are optional)
     //const profile = new NpProfile(privateKey, "https://orcid.org/" + orcidId, name);
