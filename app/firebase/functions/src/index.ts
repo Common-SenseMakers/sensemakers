918012569f19e6af1ad78757323377dfaf66dab1
Pepo <pepo.ospina@gmail.com>
2024-07-12 16:55:32 +0200

    add magic secret

--- app/firebase/functions/src/index.ts
+++ app/firebase/functions/src/index.ts
@@ -55,6 +55,7 @@ const secrets = [
   envRuntime.TWITTER_CLIENT_SECRET,
   envRuntime.NP_PUBLISH_RSA_PRIVATE_KEY,
   envRuntime.EMAIL_CLIENT_SECRET,
+  envRuntime.MAGIC_ADMIN_SECRET,
 ];
 
 // import { fetchNewPosts } from './posts/posts.job';
