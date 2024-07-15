b82a2246307bec175f502ceaf1d4d1b336bdd7e6
Wesley Finck <wesleyfinck@gmail.com>
2024-07-15 13:21:00 -0700

    update test html to use render function

--- app/webapp/test/email.test.ts
+++ app/webapp/test/email.test.ts
@@ -1,7 +1,5 @@
+import { renderEmail } from '../src/email';
 import { getMockPost } from '../src/mocks/posts.mock';
 
-const renderEmail = require('../build/render-email.js').renderEmail;
-
 const root = document.getElementById('root');
-const posts = [getMockPost()];
-root ? (root.innerHTML = renderEmail(posts)) : null;
+root ? (root.innerHTML = renderEmail([getMockPost()])) : null;
