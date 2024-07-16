c64c5b0fde320a1af83e384bdf85c02d8c91c706
Wesley Finck <wesleyfinck@gmail.com>
2024-07-16 11:09:14 -0700

    make Email Template manual render test render from bundle directly or React Directly depending on boolean

--- app/webapp/test/email.test.ts
+++ app/webapp/test/email.test.ts
@@ -1,5 +1,29 @@
-import { renderEmail } from '../src/email';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { EmailTemplate } from '../src/email/EmailTemplate';
 import { getMockPost } from '../src/mocks/posts.mock';
 
+const pkg = require('../build/render-email.js');
+const { renderEmail } = pkg;
+
+const USE_BUNDLE = true;
+
 const root = document.getElementById('root');
-root ? (root.innerHTML = renderEmail([getMockPost()])) : null;
+
+if (USE_BUNDLE) {
+  root
+    ? (root.innerHTML = renderEmail([
+        getMockPost(),
+        getMockPost(),
+        getMockPost(),
+      ]))
+    : null;
+} else {
+  ReactDOM.render(
+    React.createElement(EmailTemplate, {
+      posts: [getMockPost(), getMockPost()],
+    }),
+    root
+  );
+}
