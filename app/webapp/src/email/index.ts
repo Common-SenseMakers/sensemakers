2f066e353d719cf02a871153a6fa4757ad22f715
Wesley Finck <wesleyfinck@gmail.com>
2024-07-16 16:51:28 -0700

    include notification frequency and autopost options in the renderEmail function

--- app/webapp/src/email/index.ts
+++ app/webapp/src/email/index.ts
@@ -2,13 +2,25 @@ import { render } from '@react-email/components';
 import React from 'react';
 import { ServerStyleSheet } from 'styled-components';
 
+import { NotificationFreq } from '../shared/types/types.notifications';
 import { AppPostFull } from '../shared/types/types.posts';
+import { AutopostOption } from '../shared/types/types.user';
 import { EmailTemplate } from './EmailTemplate';
 
-export const renderEmail = (posts: AppPostFull[]) => {
+export const renderEmail = (
+  posts: AppPostFull[],
+  notificationFrequency: NotificationFreq,
+  autopostOption: AutopostOption
+) => {
   const sheet = new ServerStyleSheet();
   const html = render(
-    sheet.collectStyles(React.createElement(EmailTemplate, { posts }))
+    sheet.collectStyles(
+      React.createElement(EmailTemplate, {
+        posts,
+        notificationFrequency,
+        autopostOption,
+      })
+    )
   );
   const styleTags = sheet.getStyleTags();
   return `${styleTags}${html}`;
