70f7c1c024d7f00642733348e0be17b99735a404
Wesley Finck <wesleyfinck@gmail.com>
2024-07-16 11:08:36 -0700

    use styled-components SSR to include styles in html string

--- app/webapp/src/email/index.ts
+++ app/webapp/src/email/index.ts
@@ -1,8 +1,15 @@
 import { render } from '@react-email/components';
 import React from 'react';
+import { ServerStyleSheet } from 'styled-components';
 
 import { AppPostFull } from '../shared/types/types.posts';
 import { EmailTemplate } from './EmailTemplate';
 
-export const renderEmail = (posts: AppPostFull[]) =>
-  render(React.createElement(EmailTemplate, { posts }));
+export const renderEmail = (posts: AppPostFull[]) => {
+  const sheet = new ServerStyleSheet();
+  const html = render(
+    sheet.collectStyles(React.createElement(EmailTemplate, { posts }))
+  );
+  const styleTags = sheet.getStyleTags();
+  return `${styleTags}${html}`;
+};
