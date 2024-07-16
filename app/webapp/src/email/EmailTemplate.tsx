4cdfc8a8c70a5fbe51a22920dd4fbda85059e5d8
Wesley Finck <wesleyfinck@gmail.com>
2024-07-16 16:46:28 -0700

    create email template wrapper component to handle contexts

--- app/webapp/src/email/EmailTemplate.tsx
+++ app/webapp/src/email/EmailTemplate.tsx
@@ -1,35 +1,84 @@
-import { Heading, Html } from '@react-email/components';
-import { I18nextProvider } from 'react-i18next';
+import { Html } from '@react-email/components';
+import { Box, Heading as GrommetHeading, Image } from 'grommet';
 
-import { ToastsContext } from '../app/ToastsContext';
-import { GlobalStyles } from '../app/layout/GlobalStyles';
-import { i18n } from '../i18n/i18n';
 import { PostCard } from '../post/PostCard';
+import { NotificationFreq } from '../shared/types/types.notifications';
 import { AppPostFull } from '../shared/types/types.posts';
-import { ResponsiveApp } from '../ui-components/ResponsiveApp';
-import { ThemedApp } from '../ui-components/ThemedApp';
+import { AutopostOption } from '../shared/types/types.user';
+import { AppButton } from '../ui-components';
+import { useThemeContext } from '../ui-components/ThemedApp';
 
-export function EmailTemplate(props: { posts: AppPostFull[] }) {
+export function EmailTemplate(props: {
+  posts: AppPostFull[];
+  notificationFrequency: NotificationFreq;
+  autopostOption: AutopostOption;
+}) {
+  const { constants } = useThemeContext();
   const { posts } = props;
+
+  const heading =
+    (() => {
+      if (props.autopostOption === AutopostOption.MANUAL) {
+        return `You have ${posts.length} potential nanopublication${posts.length > 1 ? 's' : ''} ready for review`;
+      } else {
+        return `We've automatically nanopublished ${posts.length} post${posts.length > 1 ? 's' : ''}`;
+      }
+    })() +
+    (() => {
+      if (props.notificationFrequency === NotificationFreq.Daily) {
+        return ' today.';
+      } else if (props.notificationFrequency === NotificationFreq.Weekly) {
+        return ' this week.';
+      } else if (props.notificationFrequency === NotificationFreq.Monthly) {
+        return ' this month.';
+      }
+      return '';
+    })();
   return (
-    <>
-      <I18nextProvider i18n={i18n}>
-        <ToastsContext>
-          <GlobalStyles />
-          <ThemedApp>
-            <ResponsiveApp>
-              <Html lang="en">
-                <Heading as="h1">
-                  You have {posts.length} potential nanopublications to review.
-                </Heading>
-                {posts.map((post) => (
-                  <PostCard key={post.id} post={post} handleClick={() => {}} />
-                ))}
-              </Html>
-            </ResponsiveApp>
-          </ThemedApp>
-        </ToastsContext>
-      </I18nextProvider>
-    </>
+    <Html lang="en">
+      <Box
+        align="center"
+        pad="large"
+        style={{ backgroundColor: constants.colors.shade }}>
+        <Box align="center" pad={{ bottom: 'medium' }}>
+          <Box
+            width="80px"
+            height="80px"
+            round="full"
+            background="white"
+            align="center"
+            justify="center"
+            margin={{ bottom: 'medium' }}>
+            <Image
+              src="/icons/logo.png"
+              alt="Logo"
+              width="50px"
+              height="50px"
+            />
+          </Box>
+          <GrommetHeading level="2" margin="medium">
+            {heading}
+          </GrommetHeading>
+        </Box>
+
+        {posts.map((post) => (
+          <Box
+            key={post.id}
+            margin={{ bottom: 'medium' }}
+            align="center"
+            width={{ max: '600px' }}>
+            <PostCard key={post.id} post={post} handleClick={() => {}} />
+            <Box align="center" margin={{ top: 'medium' }} width="auto">
+              <AppButton
+                primary
+                label="Review Post"
+                onClick={() => {}}
+                margin={{ top: 'medium' }}
+              />
+            </Box>
+          </Box>
+        ))}
+      </Box>
+    </Html>
   );
 }
