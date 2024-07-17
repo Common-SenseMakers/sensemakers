e9fbba93f6f3fa8e486287e1a6ecfd0b840206d3
Wesley Finck <wesleyfinck@gmail.com>
2024-07-17 11:49:40 -0700

    make test entry point easier to configure

--- app/webapp/test/email.test.ts
+++ app/webapp/test/email.test.ts
@@ -9,25 +9,30 @@ import { AutopostOption } from '../src/shared/types/types.user';
 const pkg = require('../build/index.js');
 const { renderEmail } = pkg;
 
-const USE_BUNDLE = true;
+const USE_BUNDLE = false;
+const NUM_POSTS = 3;
+const NOTIFICATION_FREQUENCY = NotificationFreq.Daily;
+const AUTOPOST_OPTION = AutopostOption.MANUAL;
+
+const posts = [getMockPost(), getMockPost(), getMockPost(), getMockPost(), getMockPost(), getMockPost(), getMockPost(), getMockPost()];
 
 const root = document.getElementById('root');
 
 if (USE_BUNDLE) {
   root
     ? (root.innerHTML = renderEmail(
-        [getMockPost(), getMockPost(), getMockPost()],
-        NotificationFreq.Daily,
-        AutopostOption.MANUAL
+        posts.slice(0, NUM_POSTS),
+        NOTIFICATION_FREQUENCY,
+        AUTOPOST_OPTION
       ).html)
     : null;
 } else {
   root
     ? (root.innerHTML = render(
         React.createElement(EmailTemplateWrapper, {
-          posts: [getMockPost(), getMockPost(), getMockPost(), getMockPost()],
-          notificationFrequency: NotificationFreq.Daily,
-          autopostOption: AutopostOption.MANUAL,
+          posts: posts.slice(0, NUM_POSTS),
+          notificationFrequency: NOTIFICATION_FREQUENCY,
+          autopostOption: AUTOPOST_OPTION,
         })
       ))
     : null;
