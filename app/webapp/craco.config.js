e5db1efcbe73b0a1e89e02f56b261bf172e1270f
Pepo <pepo.ospina@gmail.com>
2024-07-18 17:32:36 +0200

    wip

--- app/webapp/craco.config.js
+++ app/webapp/craco.config.js
@@ -1,29 +1,38 @@
 const webpack = require('webpack');
 
+const plugins = [
+  new webpack.DefinePlugin({
+    process: {
+      browser: true,
+      env: {
+        NODE_ENV: '"development"',
+        FUNCTIONS_BASE: '"http://127.0.0.1:5001/demo-sensenets/us-central1"',
+        ORCID_CLIENT_ID: '"APP-M1QE4V5MBUYC7Y54"',
+        TWITTER_CLIENT_ID: '"N0czb28xek5ldjRxdWpIRVVvZFg6MTpjaQ"',
+        FB_PROJECTID: '"demo-sensenets"',
+        PROJECT_TWITTER_ACCOUNT: '"rtk254"',
+        APP_BUILD: true,
+      },
+    },
+  }),
+];
+
 module.exports = {
   webpack: {
-    configure: {
-      resolve: {
-        fallback: {},
-      },
-      plugins: [
-        new webpack.DefinePlugin({
-          process: {
-            browser: true,
-            env: {
-              NODE_ENV: '"development"',
-              FUNCTIONS_BASE:
-                '"http://127.0.0.1:5001/demo-sensenets/us-central1"',
-              ORCID_CLIENT_ID: '"APP-M1QE4V5MBUYC7Y54"',
-              TWITTER_CLIENT_ID: '"N0czb28xek5ldjRxdWpIRVVvZFg6MTpjaQ"',
-              FB_PROJECTID: '"demo-sensenets"',
-              PROJECT_TWITTER_ACCOUNT: '"rtk254"',
-              APP_BUILD: true,
-            },
+    configure: (webpackConfig) => {
+      return {
+        ...webpackConfig,
+        resolve: {
+          ...webpackConfig.resolve,
+          alias: {
+            ...webpackConfig.resolve.alias,
+            'strip-ansi': require.resolve('strip-ansi-cjs'),
           },
-        }),
-      ],
+        },
+        plugins: plugins,
+      };
     },
   },
-  plugins: [],
+  // Export plugins for modification in production config
+  plugins: plugins,
 };
--- app/webapp/craco.config.prod.js
+++ app/webapp/craco.config.prod.js
@@ -1,93 +1,71 @@
 const base = require('./craco.config');
 const webpack = require('webpack');
-const orgPlugins = base.webpack.configure.plugins;
 
-const ix = orgPlugins.findIndex((e) => {
-  return e.definitions !== undefined;
-});
-const definePlugin = orgPlugins[ix];
+// Access the plugins directly
+const { plugins } = base;
+const ix = plugins.findIndex((e) => e.definitions !== undefined);
+const definePlugin = plugins[ix];
 
-/** remove original define plugin */
-base.webpack.configure.plugins.splice(ix, 1);
+// Remove original define plugin
+plugins.splice(ix, 1);
 
-/** new copy of plugins without the org define one */
-const newPlugins = [...base.webpack.configure.plugins];
+const projectSpecificEnv = (() => {
+  if (process.env.FB_PROJECT === 'dev') {
+    return {
+      FUNCTIONS_BASE: '"https://us-central1-sensenets-dev.cloudfunctions.net"',
+      FB_APIKEY: '"AIzaSyDTGlezOv68B6_sZHbUZP8xrvKgCaO614c"',
+      FB_AUTHDOMAIN: '"sensenets-dev.firebaseapp.com"',
+      FB_PROJECTID: '"sensenets-dev"',
+      FB_STORAGE_BUCKET: '"sensenets-dev.appspot.com"',
+      FB_MESSAGING_SENDER_ID: '"943634188972"',
+      FB_APPID: '"1:943634188972:web:7c68eb87a4e517af63ad09"',
+      APP_BUILD: true,
+    };
+  }
 
-if (!process.env.FB_PROJECT) {
-  throw new Error('FB_PROJECT is not set');
-}
+  if (process.env.FB_PROJECT === 'staging') {
+    return {
+      FUNCTIONS_BASE:
+        '"https://us-central1-sensenets-staging.cloudfunctions.net"',
+      FB_APIKEY: '"AIzaSyBKsxHV-pLHsUKfSyKafyYlI2huHBogAz4"',
+      FB_AUTHDOMAIN: '"sensenets-staging.firebaseapp.com"',
+      FB_PROJECTID: '"sensenets-staging"',
+      FB_STORAGE_BUCKET: '"sensenets-staging.appspot.com"',
+      FB_MESSAGING_SENDER_ID: '"999821808505"',
+      FB_APPID: '"1:999821808505:web:27c9f570bcd19f6e7755a8"',
+    };
+  }
 
-if (process.env.FB_PROJECT === 'dev') {
-  base.webpack.configure.plugins = [
-    ...newPlugins,
-    new webpack.DefinePlugin({
-      ...definePlugin.definitions,
-      process: {
-        ...definePlugin.definitions.process,
-        env: {
-          ...definePlugin.definitions.process.env,
-          NODE_ENV: '"development"',
-          FUNCTIONS_BASE:
-            '"https://us-central1-sensenets-dev.cloudfunctions.net"',
-          FB_APIKEY: '"AIzaSyDTGlezOv68B6_sZHbUZP8xrvKgCaO614c"',
-          FB_AUTHDOMAIN: '"sensenets-dev.firebaseapp.com"',
-          FB_PROJECTID: '"sensenets-dev"',
-          FB_STORAGE_BUCKET: '"sensenets-dev.appspot.com"',
-          FB_MESSAGING_SENDER_ID: '"943634188972"',
-          FB_APPID: '"1:943634188972:web:7c68eb87a4e517af63ad09"',
-          APP_BUILD: true,
-        },
+  if (process.env.FB_PROJECT === 'production') {
+    return {
+      FUNCTIONS_BASE: '"https://us-central1-sensenets-prod.cloudfunctions.net"',
+      FB_APIKEY: '"AIzaSyCEmcE2N789UbzcFUwxjsQeq3GR3CzFXnA"',
+      FB_AUTHDOMAIN: '"sensenets-prod.firebaseapp.com"',
+      FB_PROJECTID: '"sensenets-prod"',
+      FB_STORAGE_BUCKET: '"sensenets-prod.appspot.com"',
+      FB_MESSAGING_SENDER_ID: '"764998496280"',
+      FB_APPID: '"1:764998496280:web:0f3d0515674eb53ca7936d"',
+    };
+  }
+})();
+
+// Define new plugins array with modified configuration
+
+// Setup development-specific plugins
+plugins.push(
+  new webpack.DefinePlugin({
+    ...definePlugin.definitions,
+    process: {
+      ...definePlugin.definitions.process,
+      env: {
+        ...definePlugin.definitions.process.env,
+        NODE_ENV: '"development"',
+        ...projectSpecificEnv,
       },
-    }),
-  ];
-}
+    },
+  })
+);
 
-if (process.env.FB_PROJECT === 'staging') {
-  base.webpack.configure.plugins = [
-    ...newPlugins,
-    new webpack.DefinePlugin({
-      ...definePlugin.definitions,
-      process: {
-        ...definePlugin.definitions.process,
-        env: {
-          ...definePlugin.definitions.process.env,
-          NODE_ENV: '"production"',
-          FUNCTIONS_BASE:
-            '"https://us-central1-sensenets-staging.cloudfunctions.net"',
-          FB_APIKEY: '"AIzaSyBKsxHV-pLHsUKfSyKafyYlI2huHBogAz4"',
-          FB_AUTHDOMAIN: '"sensenets-staging.firebaseapp.com"',
-          FB_PROJECTID: '"sensenets-staging"',
-          FB_STORAGE_BUCKET: '"sensenets-staging.appspot.com"',
-          FB_MESSAGING_SENDER_ID: '"999821808505"',
-          FB_APPID: '"1:999821808505:web:27c9f570bcd19f6e7755a8"',
-        },
-      },
-    }),
-  ];
-}
-
-if (process.env.FB_PROJECT === 'production') {
-  base.webpack.configure.plugins = [
-    ...newPlugins,
-    new webpack.DefinePlugin({
-      ...definePlugin.definitions,
-      process: {
-        ...definePlugin.definitions.process,
-        env: {
-          ...definePlugin.definitions.process.env,
-          NODE_ENV: '"production"',
-          FUNCTIONS_BASE:
-            '"https://us-central1-sensenets-prod.cloudfunctions.net"',
-          FB_APIKEY: '"AIzaSyCEmcE2N789UbzcFUwxjsQeq3GR3CzFXnA"',
-          FB_AUTHDOMAIN: '"sensenets-prod.firebaseapp.com"',
-          FB_PROJECTID: '"sensenets-prod"',
-          FB_STORAGE_BUCKET: '"sensenets-prod.appspot.com"',
-          FB_MESSAGING_SENDER_ID: '"764998496280"',
-          FB_APPID: '"1:764998496280:web:0f3d0515674eb53ca7936d"',
-        },
-      },
-    }),
-  ];
-}
+// Repeat the above if-block for staging and production with their respective settings
 
 module.exports = base;
