b088b95ef0aef69f1f7df850884170d0377ac541
Pepo <pepo.ospina@gmail.com>
2024-07-18 18:04:20 +0200

    changed craco config webpack config

--- app/webapp/craco.config.js
+++ app/webapp/craco.config.js
@@ -1,17 +1,26 @@
 const webpack = require('webpack');
 
-const plugins = [
+const getPlugins = (env) => [
   new webpack.DefinePlugin({
     process: {
       browser: true,
       env: {
-        NODE_ENV: '"development"',
-        FUNCTIONS_BASE: '"http://127.0.0.1:5001/demo-sensenets/us-central1"',
-        ORCID_CLIENT_ID: '"APP-M1QE4V5MBUYC7Y54"',
-        TWITTER_CLIENT_ID: '"N0czb28xek5ldjRxdWpIRVVvZFg6MTpjaQ"',
-        FB_PROJECTID: '"demo-sensenets"',
-        PROJECT_TWITTER_ACCOUNT: '"rtk254"',
-        APP_BUILD: true,
+        NODE_ENV: JSON.stringify(env.NODE_ENV || 'development'),
+        FUNCTIONS_BASE: JSON.stringify(
+          env.FUNCTIONS_BASE ||
+            'http://127.0.0.1:5001/demo-sensenets/us-central1'
+        ),
+        ORCID_CLIENT_ID: JSON.stringify(
+          env.ORCID_CLIENT_ID || 'APP-M1QE4V5MBUYC7Y54'
+        ),
+        TWITTER_CLIENT_ID: JSON.stringify(
+          env.TWITTER_CLIENT_ID || 'N0czb28xek5ldjRxdWpIRVVvZFg6MTpjaQ'
+        ),
+        FB_PROJECTID: JSON.stringify(env.FB_PROJECTID || 'demo-sensenets'),
+        PROJECT_TWITTER_ACCOUNT: JSON.stringify(
+          env.PROJECT_TWITTER_ACCOUNT || 'rtk254'
+        ),
+        ...env,
       },
     },
   }),
@@ -19,7 +28,7 @@ const plugins = [
 
 module.exports = {
   webpack: {
-    configure: (webpackConfig) => {
+    configure: (webpackConfig, { env, paths }) => {
       return {
         ...webpackConfig,
         resolve: {
@@ -29,10 +38,8 @@ module.exports = {
             'strip-ansi': require.resolve('strip-ansi-cjs'),
           },
         },
-        plugins: plugins,
+        plugins: [...webpackConfig.plugins, ...getPlugins(process.env)],
       };
     },
   },
-  // Export plugins for modification in production config
-  plugins: plugins,
 };
--- app/webapp/craco.config.prod.js
+++ app/webapp/craco.config.prod.js
@@ -1,71 +1,64 @@
-const base = require('./craco.config');
-const webpack = require('webpack');
-
-// Access the plugins directly
-const { plugins } = base;
-const ix = plugins.findIndex((e) => e.definitions !== undefined);
-const definePlugin = plugins[ix];
-
-// Remove original define plugin
-plugins.splice(ix, 1);
+const baseConfig = require('./craco.config');
 
 const projectSpecificEnv = (() => {
   if (process.env.FB_PROJECT === 'dev') {
     return {
-      FUNCTIONS_BASE: '"https://us-central1-sensenets-dev.cloudfunctions.net"',
-      FB_APIKEY: '"AIzaSyDTGlezOv68B6_sZHbUZP8xrvKgCaO614c"',
-      FB_AUTHDOMAIN: '"sensenets-dev.firebaseapp.com"',
-      FB_PROJECTID: '"sensenets-dev"',
-      FB_STORAGE_BUCKET: '"sensenets-dev.appspot.com"',
-      FB_MESSAGING_SENDER_ID: '"943634188972"',
-      FB_APPID: '"1:943634188972:web:7c68eb87a4e517af63ad09"',
-      APP_BUILD: true,
+      FUNCTIONS_BASE: 'https://us-central1-sensenets-dev.cloudfunctions.net',
+      FB_APIKEY: 'AIzaSyDTGlezOv68B6_sZHbUZP8xrvKgCaO614c',
+      FB_AUTHDOMAIN: 'sensenets-dev.firebaseapp.com',
+      FB_PROJECTID: 'sensenets-dev',
+      FB_STORAGE_BUCKET: 'sensenets-dev.appspot.com',
+      FB_MESSAGING_SENDER_ID: '943634188972',
+      FB_APPID: '1:943634188972:web:7c68eb87a4e517af63ad09',
     };
   }
 
   if (process.env.FB_PROJECT === 'staging') {
     return {
       FUNCTIONS_BASE:
-        '"https://us-central1-sensenets-staging.cloudfunctions.net"',
-      FB_APIKEY: '"AIzaSyBKsxHV-pLHsUKfSyKafyYlI2huHBogAz4"',
-      FB_AUTHDOMAIN: '"sensenets-staging.firebaseapp.com"',
-      FB_PROJECTID: '"sensenets-staging"',
-      FB_STORAGE_BUCKET: '"sensenets-staging.appspot.com"',
-      FB_MESSAGING_SENDER_ID: '"999821808505"',
-      FB_APPID: '"1:999821808505:web:27c9f570bcd19f6e7755a8"',
+        'https://us-central1-sensenets-staging.cloudfunctions.net',
+      FB_APIKEY: 'AIzaSyBKsxHV-pLHsUKfSyKafyYlI2huHBogAz4',
+      FB_AUTHDOMAIN: 'sensenets-staging.firebaseapp.com',
+      FB_PROJECTID: 'sensenets-staging',
+      FB_STORAGE_BUCKET: 'sensenets-staging.appspot.com',
+      FB_MESSAGING_SENDER_ID: '999821808505',
+      FB_APPID: '1:999821808505:web:27c9f570bcd19f6e7755a8',
     };
   }
 
   if (process.env.FB_PROJECT === 'production') {
     return {
-      FUNCTIONS_BASE: '"https://us-central1-sensenets-prod.cloudfunctions.net"',
-      FB_APIKEY: '"AIzaSyCEmcE2N789UbzcFUwxjsQeq3GR3CzFXnA"',
-      FB_AUTHDOMAIN: '"sensenets-prod.firebaseapp.com"',
-      FB_PROJECTID: '"sensenets-prod"',
-      FB_STORAGE_BUCKET: '"sensenets-prod.appspot.com"',
-      FB_MESSAGING_SENDER_ID: '"764998496280"',
-      FB_APPID: '"1:764998496280:web:0f3d0515674eb53ca7936d"',
+      FUNCTIONS_BASE: 'https://us-central1-sensenets-prod.cloudfunctions.net',
+      FB_APIKEY: 'AIzaSyCEmcE2N789UbzcFUwxjsQeq3GR3CzFXnA',
+      FB_AUTHDOMAIN: 'sensenets-prod.firebaseapp.com',
+      FB_PROJECTID: 'sensenets-prod',
+      FB_STORAGE_BUCKET: 'sensenets-prod.appspot.com',
+      FB_MESSAGING_SENDER_ID: '764998496280',
+      FB_APPID: '1:764998496280:web:0f3d0515674eb53ca7936d',
     };
   }
+
+  return {};
 })();
 
-// Define new plugins array with modified configuration
+module.exports = {
+  ...baseConfig,
+  webpack: {
+    ...baseConfig.webpack,
+    configure: (webpackConfig, { env, paths }) => {
+      // Get the modified config from the base configuration
+      const modifiedConfig = baseConfig.webpack.configure(webpackConfig, {
+        env,
+        paths,
+      });
 
-// Setup development-specific plugins
-plugins.push(
-  new webpack.DefinePlugin({
-    ...definePlugin.definitions,
-    process: {
-      ...definePlugin.definitions.process,
-      env: {
-        ...definePlugin.definitions.process.env,
-        NODE_ENV: '"development"',
+      // Override process.env with production-specific values
+      Object.assign(process.env, {
+        NODE_ENV: 'production',
         ...projectSpecificEnv,
-      },
+      });
+
+      return modifiedConfig;
     },
-  })
-);
-
-// Repeat the above if-block for staging and production with their respective settings
-
-module.exports = base;
+  },
+};
