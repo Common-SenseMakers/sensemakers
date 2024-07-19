dc1275b746c22e75b9dd3bc008327a85f16dfc7d
Pepo <pepo.ospina@gmail.com>
2024-07-19 11:44:07 +0200

    revert craco config file changes

--- app/webapp/craco.config.js
+++ app/webapp/craco.config.js
@@ -1,34 +1,28 @@
 const webpack = require('webpack');
 
-const getPlugins = () => [
-  new webpack.DefinePlugin({
-    process: {
-      browser: true,
-      env: {
-        NODE_ENV: '"development"',
-        FUNCTIONS_BASE: '"http://127.0.0.1:5001/demo-sensenets/us-central1"',
-        ORCID_CLIENT_ID: '"APP-M1QE4V5MBUYC7Y54"',
-        TWITTER_CLIENT_ID: '"N0czb28xek5ldjRxdWpIRVVvZFg6MTpjaQ"',
-        FB_PROJECTID: '"demo-sensenets"',
-        PROJECT_TWITTER_ACCOUNT: '"rtk254"',
-      },
-    },
-  }),
-];
-
 module.exports = {
   webpack: {
-    configure: (webpackConfig, { env, paths }) => {
-      return {
-        ...webpackConfig,
-        resolve: {
-          ...webpackConfig.resolve,
-          alias: {
-            ...webpackConfig.resolve.alias,
+    configure: {
+      resolve: {
+        fallback: {},
+      },
+      plugins: [
+        new webpack.DefinePlugin({
+          process: {
+            browser: true,
+            env: {
+              NODE_ENV: '"development"',
+              FUNCTIONS_BASE:
+                '"http://127.0.0.1:5001/demo-sensenets/us-central1"',
+              ORCID_CLIENT_ID: '"APP-M1QE4V5MBUYC7Y54"',
+              TWITTER_CLIENT_ID: '"N0czb28xek5ldjRxdWpIRVVvZFg6MTpjaQ"',
+              FB_PROJECTID: '"demo-sensenets"',
+              PROJECT_TWITTER_ACCOUNT: '"rtk254"',
+            },
           },
-        },
-        plugins: [...webpackConfig.plugins, ...getPlugins(process.env)],
-      };
+        }),
+      ],
     },
   },
+  plugins: [],
 };
