382fbd566206a1b1dbd57c261f6838abfceb2219
Wesley Finck <wesleyfinck@gmail.com>
2024-07-24 15:03:19 -0700

    include link to published nanopub

--- app/webapp/src/email/PostCardEmail.tsx
+++ app/webapp/src/email/PostCardEmail.tsx
@@ -38,7 +38,7 @@ export const PostCardEmail = ({ post }: PostCardEmailProps) => {
   const size = 12;
 
   const { keywords, references } = parsePostSemantics(post);
-  const { nanopubUrl, pending } = getPostStatuses(post);
+  const { published, pending, nanopubUrl } = getPostStatuses(post);
 
   return (
     <Section style={content}>
@@ -99,34 +99,36 @@ export const PostCardEmail = ({ post }: PostCardEmailProps) => {
             For Review
           </Text>
         )}
-        {nanopubUrl && (
-          <Text
-            style={{
-              borderRadius: '4px',
-              border: 'none',
-              padding: '0px 4px',
-              margin: '0px',
-              fontSize: '16px',
-              backgroundColor: '#d2e8df',
-              color: '#058153',
-              float: 'right',
-            }}>
-            Autopublished
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width={size}
-              height={size}
-              viewBox="0 0 12 12"
-              fill="none">
-              <path
-                d="M5.0999 3.2999H3.2999C2.80285 3.2999 2.3999 3.70285 2.3999 4.1999V8.6999C2.3999 9.19696 2.80285 9.5999 3.2999 9.5999H7.7999C8.29696 9.5999 8.6999 9.19696 8.6999 8.6999V6.8999M6.8999 2.3999H9.5999M9.5999 2.3999V5.0999M9.5999 2.3999L5.0999 6.8999"
-                stroke="#058153"
-                strokeWidth="1.2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-          </Text>
+        {published && (
+          <Link href={nanopubUrl}>
+            <Text
+              style={{
+                borderRadius: '4px',
+                border: 'none',
+                padding: '0px 4px',
+                margin: '0px',
+                fontSize: '16px',
+                backgroundColor: '#d2e8df',
+                color: '#058153',
+                float: 'right',
+              }}>
+              Autopublished
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width={size}
+                height={size}
+                viewBox="0 0 12 12"
+                fill="none">
+                <path
+                  d="M5.0999 3.2999H3.2999C2.80285 3.2999 2.3999 3.70285 2.3999 4.1999V8.6999C2.3999 9.19696 2.80285 9.5999 3.2999 9.5999H7.7999C8.29696 9.5999 8.6999 9.19696 8.6999 8.6999V6.8999M6.8999 2.3999H9.5999M9.5999 2.3999V5.0999M9.5999 2.3999L5.0999 6.8999"
+                  stroke="#058153"
+                  strokeWidth="1.2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </Text>
+          </Link>
         )}
       </div>
       <LabelsRow
