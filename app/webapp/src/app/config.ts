c78ae4cccd8037c674a9d3c312c2f6dac2940e5b
Pepo <pepo.ospina@gmail.com>
2024-07-29 16:19:42 +0200

    minor fix

--- app/webapp/src/app/config.ts
+++ app/webapp/src/app/config.ts
@@ -13,6 +13,4 @@ export const WALLETCONNECT_PROJECT_ID = 'c12fdc701fd336cf8dc059f6784906bc';
 export const MAGIC_API_KEY = 'pk_live_A603B0287DAF3C97';
 
 export const NANOPUB_EXPLORER_SERVER = process.env.NANOPUB_EXPLORER_SERVER;
-('https://np.knowledgepixels.com/');
-
 export const BUILD_ID = process.env.REACT_APP_VERSION;
