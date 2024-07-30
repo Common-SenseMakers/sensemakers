a9d7d526211833ab5245abafd7ca448d41e2fbd6
Pepo <pepo.ospina@gmail.com>
2024-07-30 16:58:50 +0200

    what?

--- app/webapp/src/user-login/contexts/ConnectedUserWrapper.tsx
+++ app/webapp/src/user-login/contexts/ConnectedUserWrapper.tsx
@@ -1,7 +1,26 @@
-import { disconnect } from 'process';
-import { PropsWithChildren, createContext, useContext } from 'react';
+import { Box, Text } from 'grommet';
+import { t } from 'i18next';
+import {
+  Dispatch,
+  PropsWithChildren,
+  SetStateAction,
+  createContext,
+  useContext,
+  useState,
+} from 'react';
+import { Trans } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
 
+import { RobotIcon } from '../../app/icons/RobotIcon';
+import { I18Keys } from '../../i18n/i18n';
+import { AbsoluteRoutes } from '../../route.names';
+import { useEffect } from '../../shared/emailRenderer';
+import { AppButton, AppHeading, AppModal } from '../../ui-components';
+import { AppCheckBox } from '../../ui-components/AppCheckBox';
+import { AppParagraph } from '../../ui-components/AppParagraph';
+import { BoxCentered } from '../../ui-components/BoxCentered';
 import { UserPostsContext } from '../../user-home/UserPostsContext';
+import { usePersist } from '../../utils/use.persist';
 import { AccountContext } from './AccountContext';
 import { DisconnectUserContext } from './DisconnectUserContext';
 import { OrcidContext } from './platforms/OrcidContext';
@@ -12,11 +31,18 @@ import { SignerContext } from './signer/SignerContext';
 
 const DEBUG = false;
 
-export type ConnectedUserContextType = {};
+export interface ConnectedUserContextType {
+  reviewAutopostIntention: boolean;
+  setReviewAutopostIntention: Dispatch<SetStateAction<boolean>>;
+}
 
 const ConnectedUserWrapperValue = createContext<
   ConnectedUserContextType | undefined
 >(undefined);
+
+export const FIRST_PUBLISHED_FLAG = 'firstPublished';
+export const AUTO_POST_INVITE_DISABLED = 'autopostInviteDisabled';
+export const REVIEW_AUTOPOST_INTENTION = 'reviewAutopostIntention';
 
 /**
  * A wrapper of all context related to the connected user and its connection
@@ -25,8 +51,116 @@ const ConnectedUserWrapperValue = createContext<
  * Hooks designed ot be consumed are all implemented in the ConnectedUserContext
  */
 export const ConnectedUserWrapper = (props: PropsWithChildren) => {
+  const navigate = useNavigate();
+
+  const [firstPublished, setFirstPublished] = usePersist<boolean>(
+    FIRST_PUBLISHED_FLAG,
+    false
+  );
+  const [showInvite, setShowInvite] = useState(false);
+
+  const [autopostInviteDisabled, setAutopostInviteDisabled] =
+    usePersist<boolean>(AUTO_POST_INVITE_DISABLED, false);
+
+  const [reviewAutopostIntention, setReviewAutopostIntention] =
+    useState<boolean>(false);
+
+  const [dontShowAgain, setDontShowAgain] = useState(false);
+
+  /** count time and open autopost invite modal */
+  useEffect(() => {
+    if (firstPublished && !autopostInviteDisabled) {
+      setTimeout(() => {
+        setShowInvite(true);
+      }, 2500);
+    }
+  }, [firstPublished, autopostInviteDisabled]);
+
+  const inviteAutopostModal = (() => {
+    if (showInvite) {
+      return (
+        <AppModal type="small" onModalClosed={() => setShowInvite(false)}>
+          <>
+            <Box style={{ flexGrow: 1 }} justify="center">
+              <Box align="center">
+                <BoxCentered
+                  style={{
+                    height: '80px',
+                    width: '80px',
+                    borderRadius: '40px',
+                    backgroundColor: '#CEE2F2',
+                  }}
+                  margin={{ bottom: '16px' }}>
+                  <RobotIcon size={40}></RobotIcon>
+                </BoxCentered>
+                <AppHeading level={3} style={{ textAlign: 'center' }}>
+                  <Trans
+                    i18nKey={I18Keys.autopostInviteTitle}
+                    components={{ br: <br></br> }}></Trans>
+                </AppHeading>
+                <AppParagraph
+                  style={{
+                    marginTop: '8px',
+                    width: '100%',
+                    textAlign: 'center',
+                  }}>
+                  <Trans
+                    i18nKey={I18Keys.autpostInvitePar01}
+                    components={{ b: <b></b> }}></Trans>
+                </AppParagraph>
+                <AppParagraph
+                  style={{
+                    textAlign: 'center',
+                    marginTop: '8px',
+                    marginBottom: '20px',
+                    width: '100%',
+                  }}>
+                  <Trans
+                    i18nKey={I18Keys.autopostInvitePar02}
+                    components={{ b: <b></b> }}></Trans>
+                </AppParagraph>
+                <Box direction="row" margin={{ bottom: '24px' }} gap="12px">
+                  <AppCheckBox
+                    onChange={(e) =>
+                      setAutopostInviteDisabled(e.target.checked)
+                    }
+                    checked={dontShowAgain}></AppCheckBox>
+                  <Box>
+                    <Text size="small">{t(I18Keys.dontShowAgain)}</Text>
+                  </Box>
+                </Box>
+              </Box>
+
+              <Box style={{ width: '100%' }} gap="12px" direction="row">
+                <AppButton
+                  onClick={() => notNow()}
+                  label={t(I18Keys.notNow)}
+                  style={{ width: '100%' }}></AppButton>
+                <AppButton
+                  primary
+                  onClick={() => reviewSettings()}
+                  label={t(I18Keys.reviewSettings)}
+                  style={{ width: '100%' }}></AppButton>
+              </Box>
+            </Box>
+          </>
+        </AppModal>
+      );
+    }
+  })();
+
+  const notNow = () => {
+    setShowInvite(false);
+  };
+
+  const reviewSettings = () => {
+    setReviewAutopostIntention(true);
+    navigate(AbsoluteRoutes.Settings);
+  };
+
   return (
-    <ConnectedUserWrapperValue.Provider value={{}}>
+    <ConnectedUserWrapperValue.Provider
+      value={{ reviewAutopostIntention, setReviewAutopostIntention }}>
       <AccountContext>
         <ConnectedWallet>
           <SignerContext>
@@ -34,7 +168,10 @@ export const ConnectedUserWrapper = (props: PropsWithChildren) => {
               <NanopubContext>
                 <OrcidContext>
                   <DisconnectUserContext>
-                    <UserPostsContext>{props.children}</UserPostsContext>
+                    <UserPostsContext>
+                      {props.children}
+                      {inviteAutopostModal}
+                    </UserPostsContext>
                   </DisconnectUserContext>
                 </OrcidContext>
               </NanopubContext>
