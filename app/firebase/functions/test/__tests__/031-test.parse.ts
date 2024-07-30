f4693f95c1c7a35f02f4efc8860bcc684c3ca175
Wesley Finck <wesleyfinck@gmail.com>
2024-07-30 14:36:50 -0700

    create user directly without fetching for test

--- app/firebase/functions/test/__tests__/031-test.parse.ts
+++ app/firebase/functions/test/__tests__/031-test.parse.ts
@@ -4,18 +4,21 @@ import { AppPostParsingStatus } from '../../src/@shared/types/types.posts';
 import { AppUser, PLATFORM } from '../../src/@shared/types/types.user';
 import { USE_REAL_EMAIL } from '../../src/config/config.runtime';
 import { logger } from '../../src/instances/logger';
+import { UsersHelper } from '../../src/users/users.helper';
 import { resetDB } from '../utils/db';
+import { createUsers } from '../utils/users.utils';
 import {
   _01_createAndFetchUsers,
   _02_publishTweet,
   _03_fetchAfterPublish,
 } from './reusable/create-post-fetch';
-import { USE_REAL_NANOPUB, USE_REAL_PARSER, USE_REAL_TWITTER } from './setup';
-import { testCredentials } from './test.accounts';
+import {
+  USE_REAL_NANOPUB,
+  USE_REAL_PARSER,
+  USE_REAL_TWITTER,
+  testUsers,
+} from './setup';
 import { getTestServices } from './test.services';
-
-const DEBUG_PREFIX = `030-process`;
-const DEBUG = false;
 
 describe.only('031 test parse', () => {
   const services = getTestServices({
@@ -41,11 +44,14 @@ describe.only('031 test parse', () => {
         throw new Error('TEST_THREAD_AUTHOR_ID not defined in .env.test file');
       }
 
-      const testUser = testCredentials[1];
-      user = await _01_createAndFetchUsers(services, testUser.twitter.id, {
-        DEBUG,
-        DEBUG_PREFIX,
+      const users = await services.db.run((manager) => {
+        return createUsers(services, testUsers, manager);
       });
+      user = users.find(
+        (u) =>
+          UsersHelper.getAccount(u, PLATFORM.Twitter, platformAuthorId) !==
+          undefined
+      );
     });
 
     it('gets a post (thread) from twitter and parses it', async () => {
