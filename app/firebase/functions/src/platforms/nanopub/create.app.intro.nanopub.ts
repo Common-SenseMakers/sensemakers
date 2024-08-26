eeafa38bf8629cf4c3a8abbf0e2ba8a7f4f8e915
Pepo <pepo.ospina@gmail.com>
2024-08-26 19:58:05 +0200

    proposal for intro that just approves

--- app/firebase/functions/src/platforms/nanopub/create.app.intro.nanopub.ts
+++ app/firebase/functions/src/platforms/nanopub/create.app.intro.nanopub.ts
@@ -26,7 +26,6 @@ export const buildLinkAccountsNanopub = async (
     const keyDeclarationNode = namedNode(
       `${URI.KEY_DECLARATION_URI}${ethAddress}`
     );
-    const name = 'Sensenets';
     // Create a writer and add prefixes
     const writer = new Writer({ format: 'application/trig' });
     writer.addPrefixes(URI.introPrefixes);
@@ -147,12 +146,8 @@ export const buildLinkAccountsNanopub = async (
 };
 
 export const buildAppIntroNp = async (
-  ethAddress1: string,
-  ethAddress2: string,
-  pubKey1: string,
-  pubKey2: string,
-  signature1: string,
-  signature2: string,
+  rootKeysUri: string,
+  approvedKeysUri: string,
   supersedesOptions?: SupersedesOptions
 ): Promise<Nanopub> => {
   return new Promise((resolve, reject) => {
@@ -163,14 +158,10 @@ export const buildAppIntroNp = async (
     const assertionGraph = namedNode(URI.ASSERTION_URI);
     const provenanceGraph = namedNode(URI.PROVENANCE_URI);
     const pubinfoGraph = namedNode(URI.PUBINFO_URI);
-    const keyDeclarationNode1 = namedNode(
-      `${URI.KEY_DECLARATION_URI}${ethAddress1}`
-    );
-    const keyDeclarationNode2 = namedNode(
-      `${URI.KEY_DECLARATION_URI}${ethAddress2}`
-    );
+
     const appAgentNode = namedNode(URI.COSMO_PREFIX);
     const name = 'Sensenets';
+
     // Create a writer and add prefixes
     const writer = new Writer({ format: 'application/trig' });
     writer.addPrefixes(URI.introPrefixes);
@@ -188,157 +179,20 @@ export const buildAppIntroNp = async (
       literal(name),
       assertionGraph
     );
-    writer.addQuad(
-      keyDeclarationNode1,
-      namedNode(URI.NP_DECLARED_BY),
-      appAgentNode,
-      assertionGraph
-    );
-    writer.addQuad(
-      keyDeclarationNode1,
-      namedNode(URI.NP_HAS_ALGORITHM),
-      literal('RSA'),
-      assertionGraph
-    );
-    writer.addQuad(
-      keyDeclarationNode1,
-      namedNode(URI.NP_HAS_PUBLIC_KEY),
-      literal(pubKey1),
-      assertionGraph
-    );
-
-    writer.addQuad(
-      keyDeclarationNode1,
-      namedNode(URI.PROV_WAS_DERIVED_FROM),
-      literal(ethAddress1),
-      assertionGraph
-    );
-    writer.addQuad(
-      keyDeclarationNode1,
-      namedNode(URI.NP_HAS_DERIVATION_PATH),
-      literal('/nanopub/0'),
-      assertionGraph
-    );
-    writer.addQuad(
-      keyDeclarationNode1,
-      namedNode(URI.NP_HAS_DERIVATION_STANDARD),
-      literal('BIP-32'),
-      assertionGraph
-    );
-
-    const derivationProofNode1 = namedNode(
-      `${URI.DERIVATION_PROOF_URI}${ethAddress1}`
-    );
-    writer.addQuad(
-      keyDeclarationNode1,
-      namedNode(URI.NP_HAS_DERIVATION_PROOF),
-      derivationProofNode1,
-      assertionGraph
-    );
-    writer.addQuad(
-      derivationProofNode1,
-      namedNode(URI.NP_HAS_ALGORITHM),
-      namedNode('https://eips.ethereum.org/EIPS/eip-191'),
-      assertionGraph
-    );
-    writer.addQuad(
-      derivationProofNode1,
-      namedNode(URI.NP_HAS_PUBLIC_KEY),
-      literal(ethAddress1),
-      assertionGraph
-    );
-    writer.addQuad(
-      derivationProofNode1,
-      namedNode(URI.NP_HAS_SIGNATURE_TARGET),
-      literal(`This account controls the RSA public key: ${pubKey1}`),
-      assertionGraph
-    );
-    writer.addQuad(
-      derivationProofNode1,
-      namedNode(URI.NP_HAS_SIGNATURE),
-      literal(signature1),
-      assertionGraph
-    );
-
-    //Declare the second RSA key
-
-    writer.addQuad(
-      keyDeclarationNode2,
-      namedNode(URI.NP_DECLARED_BY),
-      appAgentNode,
-      assertionGraph
-    );
-    writer.addQuad(
-      keyDeclarationNode2,
-      namedNode(URI.NP_HAS_ALGORITHM),
-      literal('RSA'),
-      assertionGraph
-    );
-    writer.addQuad(
-      keyDeclarationNode2,
-      namedNode(URI.NP_HAS_PUBLIC_KEY),
-      literal(pubKey2),
-      assertionGraph
-    );
-
-    writer.addQuad(
-      keyDeclarationNode2,
-      namedNode(URI.PROV_WAS_DERIVED_FROM),
-      literal(ethAddress2),
-      assertionGraph
-    );
-    writer.addQuad(
-      keyDeclarationNode2,
-      namedNode(URI.NP_HAS_DERIVATION_PATH),
-      literal('/nanopub/0'),
-      assertionGraph
-    );
-    writer.addQuad(
-      keyDeclarationNode2,
-      namedNode(URI.NP_HAS_DERIVATION_STANDARD),
-      literal('BIP-32'),
-      assertionGraph
-    );
-
-    const derivationProofNode2 = namedNode(
-      `${URI.DERIVATION_PROOF_URI}${ethAddress2}`
-    );
-    writer.addQuad(
-      keyDeclarationNode2,
-      namedNode(URI.NP_HAS_DERIVATION_PROOF),
-      derivationProofNode2,
-      assertionGraph
-    );
-    writer.addQuad(
-      derivationProofNode2,
-      namedNode(URI.NP_HAS_ALGORITHM),
-      namedNode('https://eips.ethereum.org/EIPS/eip-191'),
-      assertionGraph
-    );
-    writer.addQuad(
-      derivationProofNode2,
-      namedNode(URI.NP_HAS_PUBLIC_KEY),
-      literal(ethAddress2),
-      assertionGraph
-    );
-    writer.addQuad(
-      derivationProofNode2,
-      namedNode(URI.NP_HAS_SIGNATURE_TARGET),
-      literal(`This account controls the RSA public key: ${pubKey1}`),
-      assertionGraph
-    );
-    writer.addQuad(
-      derivationProofNode2,
-      namedNode(URI.NP_HAS_SIGNATURE),
-      literal(signature2),
-      assertionGraph
-    );
 
     // Add triples to the provenance graph
     writer.addQuad(
       assertionGraph,
       namedNode(URI.PROV_WAS_ATTRIBUTED_TO),
       appAgentNode,
+      provenanceGraph
+    );
+
+    //
+    writer.addQuad(
+      namedNode(rootKeysUri),
+      namedNode(URI.APPROVES_PUBLISHING_AS_US),
+      namedNode(approvedKeysUri),
       provenanceGraph
     );
 
@@ -359,31 +213,10 @@ export const buildAppIntroNp = async (
 
     writer.addQuad(
       baseNode,
-      namedNode(URI.NP_HAS_ROOT_SIGNER),
-      literal(ethAddress1),
-      pubinfoGraph
-    );
-
-    writer.addQuad(
-      baseNode,
       namedNode(URI.CREATOR),
       appAgentNode,
       pubinfoGraph
     );
-    if (supersedesOptions) {
-      writer.addQuad(
-        baseNode,
-        namedNode(URI.NP_SUPERSEDES),
-        namedNode(supersedesOptions.latest),
-        pubinfoGraph
-      );
-      writer.addQuad(
-        namedNode(URI.BASE_URI),
-        namedNode(URI.NP_HAS_ORIGNAL_VERSION),
-        namedNode(supersedesOptions.root),
-        pubinfoGraph
-      );
-    }
 
     // Instantiate Nanopub profile (ORCID and name are optional)
     //const profile = new NpProfile(privateKey, "https://orcid.org/" + orcidId, name);
