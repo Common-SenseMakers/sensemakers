7e6a37cb3a9341f7dcbd1a92f63fc54629437f36
Pepo <pepo.ospina@gmail.com>
2024-08-29 13:12:29 +0200

    added logout and privacy policy link

--- app/webapp/src/welcome/AppWelcome.tsx
+++ app/webapp/src/welcome/AppWelcome.tsx
@@ -1,4 +1,4 @@
-import { Box } from 'grommet';
+import { Anchor, Box } from 'grommet';
 import { Trans, useTranslation } from 'react-i18next';
 
 import { AppLogo } from '../app/brand/AppLogo';
@@ -15,33 +15,42 @@ export const AppWelcome = (props: {}) => {
   const content = (() => {
     if (connectMagic) {
       return (
-        <>
-          <AppHeading level="1">{t(I18Keys.introTitle)}</AppHeading>
-          <Box width="100%" height="4px"></Box>
-          <AppSubtitle>{t(I18Keys.introSubtitle)}</AppSubtitle>
-          <Box width="100%" height="16px"></Box>
-          <AppParagraph>{t(I18Keys.introParagraph1)}</AppParagraph>
-          <AppParagraph addMargin>
-            <Trans
-              i18nKey={I18Keys.introParagraph2}
-              components={{ b: <b></b> }}></Trans>
-          </AppParagraph>
-          <AppButton
-            margin={{ top: 'large' }}
-            primary
-            disabled={isConnectingMagic}
-            label={t(I18Keys.emailInputBtn)}
-            onClick={() => connectMagic(true, true)}></AppButton>
-        </>
+        <Box style={{ flexGrow: 1 }}>
+          <Box style={{ flexGrow: 1 }}>
+            <AppHeading level="1">{t(I18Keys.introTitle)}</AppHeading>
+            <Box width="100%" height="4px"></Box>
+            <AppSubtitle>{t(I18Keys.introSubtitle)}</AppSubtitle>
+            <Box width="100%" height="16px"></Box>
+            <AppParagraph>{t(I18Keys.introParagraph1)}</AppParagraph>
+            <AppParagraph addMargin>
+              <Trans
+                i18nKey={I18Keys.introParagraph2}
+                components={{ b: <b></b> }}></Trans>
+            </AppParagraph>
+            <AppButton
+              margin={{ top: 'large' }}
+              primary
+              disabled={isConnectingMagic}
+              label={t(I18Keys.emailInputBtn)}
+              onClick={() => connectMagic(true, true)}></AppButton>
+          </Box>
+          <Box align="center">
+            <Anchor href="https://sense-nets.xyz/privacy-policy">
+              Privacy Policy
+            </Anchor>
+          </Box>
+        </Box>
       );
     } else {
       return <Loading></Loading>;
     }
   })();
   return (
-    <Box pad={{ horizontal: 'medium', vertical: 'large' }}>
+    <Box
+      pad={{ horizontal: 'medium', vertical: 'large' }}
+      style={{ flexGrow: 1 }}>
       <AppLogo margin={{ bottom: 'xlarge' }}></AppLogo>
-      <Box>{content}</Box>
+      <Box style={{ flexGrow: 1 }}>{content}</Box>
     </Box>
   );
 };
