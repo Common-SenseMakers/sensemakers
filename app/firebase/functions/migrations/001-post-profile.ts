b88b49d3b841b95034573d7c3c2c09aff7a03836
Pepo <pepo.ospina@gmail.com>
2024-09-03 09:42:20 +0200

    pretier

--- app/firebase/functions/migrations/001-post-profile.ts
+++ app/firebase/functions/migrations/001-post-profile.ts
@@ -1,29 +1,51 @@
-import fs from 'fs';
-
 import { PLATFORM } from '../src/@shared/types/types.user';
-import { servicesSource } from './migrations.services';
 import { UsersHelper } from '../src/users/users.helper';
+import { servicesSource } from './migrations.services';
 
 (async () => {
-  const platformPostsIds = await servicesSource.postsManager.processing.platformPosts.getAll();
+  const platformPostsIds =
+    await servicesSource.postsManager.processing.platformPosts.getAll();
 
   console.log(`Processing ${platformPostsIds.length} platformPosts`);
 
   await Promise.all(
     platformPostsIds.map(async (platformPostsId) => {
       servicesSource.db.run(async (manager) => {
-        const platformPost = await servicesSource.postsManager.processing.platformPosts.get(platformPostsId, manager, true);
-        
-        if (platformPost.posted && platformPost.platformId === PLATFORM.Twitter) {
+        const platformPost =
+          await servicesSource.postsManager.processing.platformPosts.get(
+            platformPostsId,
+            manager,
+            true
+          );
+
+        if (
+          platformPost.posted &&
+          platformPost.platformId === PLATFORM.Twitter
+        ) {
           console.log(`Processing platformPostsId: ${platformPostsId}`);
 
-          const author = await servicesSource.users.repo.getUser(platformPost.posted.user_id, manager, true);
-          const account =  UsersHelper.getAccount(author, platformPost.platformId, platformPost.posted.user_id, true); ;
+          const author = await servicesSource.users.repo.getUser(
+            platformPost.posted.user_id,
+            manager,
+            true
+          );
+          const account = UsersHelper.getAccount(
+            author,
+            platformPost.platformId,
+            platformPost.posted.user_id,
+            true
+          );
 
-          console.log(`Processing platformPostsId: ${platformPostsId} - author: ${author.email?.email} - account: ${account.profile?.username}`);
+          console.log(
+            `Processing platformPostsId: ${platformPostsId} - author: ${author.email?.email} - account: ${account.profile?.username}`
+          );
           platformPost.posted.author = account.profile;
-          
-          await servicesSource.postsManager.processing.platformPosts.update(platformPostsId, {posted: platformPost.posted}, manager);
+
+          await servicesSource.postsManager.processing.platformPosts.update(
+            platformPostsId,
+            { posted: platformPost.posted },
+            manager
+          );
         }
       });
     })
