e3a569b8a53e37d44c71137979075529d75cb95d
Pepo <pepo.ospina@gmail.com>
2024-09-04 11:53:28 +0200

    updated restore data script

--- app/firebase/migrations/01-make-backup.sh
+++ app/firebase/migrations/01-make-backup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Makes a backup of the Firestore data in the specified project, stores
+# it in a Cloud Storage bucket, and then downloads it to a local folder.
+
 # Check if the project ID was provided as an input
 if [ "$#" -ne 1 ]; then
   echo "Usage: $0 <project-id>"
@@ -18,12 +21,13 @@ gcloud config set project $PROJECT_ID
 
 # Define the bucket name (adjust this as necessary)
 BUCKET_NAME="${PROJECT_ID}"
+CLOUD_URL=gs://$BUCKET_NAME/backups/$DATE
 
 # Create the bucket if it doesn't exist (optional step, uncomment if needed)
 # gsutil mb -p $PROJECT_ID gs://$BUCKET_NAME/
 
 # Export Firestore data to the bucket with the date as a folder
-gcloud firestore export gs://$BUCKET_NAME/backups/$DATE
+gcloud firestore export $CLOUD_URL
 
 # Check the status of the export
 if [ $? -eq 0 ]; then
@@ -31,3 +35,9 @@ if [ $? -eq 0 ]; then
 else
   echo "Error in exporting Firestore data."
 fi
+
+# import backup to local folder
+OUTDIR=./migrations/backups/$PROJECT_ID
+
+mkdir ${OUTDIR}
+gsutil -m cp -r $CLOUD_URL $OUTDIR
