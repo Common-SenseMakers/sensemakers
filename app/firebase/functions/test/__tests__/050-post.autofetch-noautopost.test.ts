5cf13fe76c34e563897e42f224bca573483d7129
Wesley Finck <wesleyfinck@gmail.com>
2024-09-11 13:17:01 -0700

    050 passing

--- app/firebase/functions/test/__tests__/050-post.autofetch-noautopost.test.ts
+++ app/firebase/functions/test/__tests__/050-post.autofetch-noautopost.test.ts
@@ -10,6 +10,7 @@ import {
   AppPostParsedStatus,
   AppPostParsingStatus,
   AppPostRepublishedStatus,
+  PostsQueryStatus,
 } from '../../src/@shared/types/types.posts';
 import { TwitterThread } from '../../src/@shared/types/types.twitter';
 import { AppUser, PLATFORM } from '../../src/@shared/types/types.user';
@@ -78,9 +79,15 @@ describe('050-autofetch-no-autopost', () => {
 
       /** read user posts */
       const postsRead = await globalTestServices.postsManager.getOfUser(
-        user.userId
+        user.userId,
+        {
+          status: PostsQueryStatus.DRAFTS,
+          fetchParams: { expectedAmount: TEST_THREADS.length + 1 + 6 },
+        }
       );
-      expect(postsRead).to.have.length(TEST_THREADS.length + 1);
+      expect(
+        postsRead.filter((post) => post.origin === PLATFORM.Twitter)
+      ).to.have.length(TEST_THREADS.length + 1);
 
       const postOfThread2 = postsRead.find((p) =>
         p.mirrors.find(
