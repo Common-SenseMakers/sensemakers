dbbc393a9f43547a85c0986e6722ccf6e5fb3064
Wesley Finck <wesleyfinck@gmail.com>
2024-10-22 18:02:22 -0700

    WIP: update triples test working

--- app/firebase/functions/scripts/014-update-collections.ts
+++ app/firebase/functions/scripts/014-update-collections.ts
@@ -1,159 +1,174 @@
+import { PLATFORM } from '../src/@shared/types/types.platforms';
 import { app } from './scripts.services';
 
 const firestore = app.firestore();
 
+if (process.env.FB_PROJECT_ID !== 'sensenets-dataset') {
+  throw new Error('Cannot run this script on other projects');
+}
+
 async function updateCollections() {
-  await updateProfiles();
-  await updatePosts();
-  await updatePlatformPosts();
-  await removeUserDocs();
+  // await updateProfiles();
+  // await updatePosts();
+  // await updatePlatformPosts();
+  // await removeUserDocs();
   await updateTriples();
 }
 
-async function updateProfiles() {
-  const profilesSnapshot = await firestore.collection('profiles').get();
+// async function updateProfiles() {
+//   const profilesSnapshot = await firestore.collection('profiles').get();
 
-  for (const doc of profilesSnapshot.docs) {
-    const data = doc.data();
-    const userId = data.profile.userId || data.profile.user_id;
-    const platformId = data.platformId;
+//   for (const doc of profilesSnapshot.docs) {
+//     const data = doc.data();
+//     const userId = data.profile.userId || data.profile.user_id;
+//     const platformId = data.platformId;
 
-    if (!userId || !platformId) continue;
+//     if (!userId || !platformId) continue;
 
-    const userDoc = await firestore
-      .collection('users')
-      .doc(userId.split(':')[1])
-      .get();
-    const userData = userDoc.data();
+//     const userDoc = await firestore
+//       .collection('users')
+//       .doc(userId.split(':')[1])
+//       .get();
+//     const userData = userDoc.data();
 
-    if (!userData) continue;
+//     if (!userData) continue;
 
-    const newProfileData = {
-      platformId,
-      profile: {
-        id: data.profile.id,
-        name: data.profile.name || data.profile.displayName || '',
-        username: data.profile.username || '',
-        avatar: data.profile.avatar || '',
-      },
-      fetched: userData.accounts[platformId]?.fetched || {},
-    };
+//     const newProfileData = {
+//       platformId,
+//       profile: {
+//         id: data.profile.id,
+//         name: data.profile.name || data.profile.displayName || '',
+//         username: data.profile.username || '',
+//         avatar: data.profile.avatar || '',
+//       },
+//       fetched: userData.accounts[platformId]?.fetched || {},
+//     };
 
-    if (platformId === 'mastodon') {
-      newProfileData.profile.username = `${data.profile.username}@${data.profile.mastodonServer || ''}`;
-      delete newProfileData.profile.mastodonServer;
-    }
+//     if (platformId === 'mastodon') {
+//       newProfileData.profile.username = `${data.profile.username}@${data.profile.mastodonServer || ''}`;
+//       //@ts-ignore
+//       delete newProfileData.profile.mastodonServer;
+//     }
 
-    const newDocId = `${platformId}:${data.profile.id}`;
-    await firestore.collection('profiles').doc(newDocId).set(newProfileData);
-    await doc.ref.delete();
-  }
+//     const newDocId = `${platformId}:${data.profile.id}`;
+//     await firestore.collection('profiles').doc(newDocId).set(newProfileData);
+//     await doc.ref.delete();
+//   }
 
-  console.log('Profiles updated');
-}
+//   console.log('Profiles updated');
+// }
 
-async function updatePosts() {
-  const postsSnapshot = await firestore.collection('posts').get();
+// async function updatePosts() {
+//   const postsSnapshot = await firestore.collection('posts').get();
 
-  for (const doc of postsSnapshot.docs) {
-    const data = doc.data();
-    const origin = data.origin;
-    const authorId = data.authorId;
+//   for (const doc of postsSnapshot.docs) {
+//     const data = doc.data();
+//     const origin = data.origin;
+//     const authorId = data.authorId;
 
-    if (!origin || !authorId) continue;
+//     if (!origin || !authorId) continue;
 
-    const userDoc = await firestore
-      .collection('users')
-      .doc(authorId.split(':')[1])
-      .get();
-    const userData = userDoc.data();
+//     const userDoc = await firestore
+//       .collection('users')
+//       .doc(authorId.split(':')[1])
+//       .get();
+//     const userData = userDoc.data();
 
-    if (!userData) continue;
+//     if (!userData) continue;
 
-    const accountInfo = userData.accounts[origin];
-    if (!accountInfo) continue;
+//     const accountInfo = userData.accounts[origin];
+//     if (!accountInfo) continue;
 
-    const updatedData = {
-      ...data,
-      generic: {
-        ...data.generic,
-        author: {
-          ...data.generic.author,
-          ...accountInfo.profile,
-        },
-      },
-      authorProfileId: `${origin}:${accountInfo.profile.id}`,
-    };
+//     const updatedData = {
+//       ...data,
+//       generic: {
+//         ...data.generic,
+//         author: {
+//           ...data.generic.author,
+//           ...accountInfo.profile,
+//         },
+//       },
+//       authorProfileId: `${origin}:${accountInfo.profile.id}`,
+//     };
 
-    delete updatedData.authorId;
+//     //@ts-ignore
+//     delete updatedData.authorId;
 
-    await doc.ref.set(updatedData);
-  }
+//     await doc.ref.set(updatedData);
+//   }
 
-  console.log('Posts updated');
-}
+//   console.log('Posts updated');
+// }
 
-async function updatePlatformPosts() {
-  const platformPostsSnapshot = await firestore
-    .collection('platformPosts')
-    .get();
+// async function updatePlatformPosts() {
+//   const platformPostsSnapshot = await firestore
+//     .collection('platformPosts')
+//     .get();
 
-  for (const doc of platformPostsSnapshot.docs) {
-    const data = doc.data();
+//   for (const doc of platformPostsSnapshot.docs) {
+//     const data = doc.data();
 
-    if (data.platformId === 'mastodon') {
-      const updatedData = {
-        ...data,
-        post_id: data.posted.post.id,
-        'posted.post_id': data.posted.post.id,
-        'posted.user_id': data.posted.post.account.id,
-      };
+//     if (data.platformId === 'mastodon') {
+//       const updatedData = {
+//         ...data,
+//         post_id: data.posted.post.id,
+//         'posted.post_id': data.posted.post.id,
+//         'posted.user_id': data.posted.post.account.id,
+//       };
 
-      await doc.ref.set(updatedData);
-    }
-  }
+//       await doc.ref.set(updatedData);
+//     }
+//   }
 
-  console.log('PlatformPosts updated');
-}
+//   console.log('PlatformPosts updated');
+// }
 
-async function removeUserDocs() {
-  const userDocs = await firestore.collection('users').get();
-  const batch = firestore.batch();
+// async function removeUserDocs() {
+//   const userDocs = await firestore.collection('users').get();
+//   const batch = firestore.batch();
 
-  userDocs.forEach((doc) => {
-    batch.delete(doc.ref);
-  });
+//   userDocs.forEach((doc) => {
+//     batch.delete(doc.ref);
+//   });
 
-  await batch.commit();
-  console.log('User documents removed');
-}
+//   await batch.commit();
+//   console.log('User documents removed');
+// }
 
 async function updateTriples() {
   const triplesSnapshot = await firestore.collection('triples').get();
 
+  let count = 0;
   for (const doc of triplesSnapshot.docs) {
+    if (count >= 5) break;
     const data = doc.data();
+    count++;
 
     if (data.authorId) {
       const updatedData = { ...data };
-      
+
       // Change createdAtMs to postCreatedAtMs
       updatedData.postCreatedAtMs = data.createdAtMs;
       delete updatedData.createdAtMs;
 
       // Update authorId to authorProfileId
       const [platformId, platformAccountId] = data.authorId.split(':');
-      
-      if (platformId === 'bluesky' || platformId === 'twitter') {
+
+      if (platformId === PLATFORM.Bluesky || platformId === PLATFORM.Twitter) {
         updatedData.authorProfileId = `${platformId}-${platformAccountId}`;
-      } else if (platformId === 'mastodon') {
-        const userDoc = await firestore.collection('users').doc(platformAccountId).get();
+      } else if (platformId === PLATFORM.Mastodon) {
+        const userDoc = await firestore
+          .collection('users')
+          .doc(data.authorId)
+          .get();
         const userData = userDoc.data();
         if (userData && userData.mastodon && userData.mastodon[0]) {
           const globalUsername = `${userData.mastodon[0].profile.username}@${userData.mastodon[0].profile.mastodonServer}`;
-          updatedData.authorProfileId = `mastodon-${globalUsername}`;
+          updatedData.authorProfileId = `${PLATFORM.Mastodon}-${globalUsername}`;
         } else {
-          console.warn(`User document not found or invalid for Mastodon user: ${platformAccountId}`);
+          console.warn(
+            `User document not found or invalid for Mastodon user: ${platformAccountId}`
+          );
           continue;
         }
       } else {
@@ -162,6 +177,8 @@ async function updateTriples() {
       }
 
       delete updatedData.authorId;
+
+      console.log('Updating triple:', { updatedData, docId: doc.id });
 
       await doc.ref.set(updatedData);
     }
