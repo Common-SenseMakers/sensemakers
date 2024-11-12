baf01d52baccf447f140ce35658ffe72d629a340
Wesley Finck <wesleyfinck@gmail.com>
2024-11-12 15:30:22 -0800

    wip: show link thumbnail if exists, otherwise ignore. removed summary

--- app/webapp/src/semantics/patterns/common/RefCard.tsx
+++ app/webapp/src/semantics/patterns/common/RefCard.tsx
@@ -1,5 +1,8 @@
-import { Anchor, Box, Paragraph, Text } from 'grommet';
+import axios from 'axios';
+import { Anchor, Box, Text } from 'grommet';
+import { useEffect, useState } from 'react';
 
+import { IFRAMELY_API_KEY, IFRAMELY_API_URL } from '../../../app/config';
 import { AppHeading } from '../../../ui-components';
 import { useThemeContext } from '../../../ui-components/ThemedApp';
 import { zoteroItemTypeDisplay } from '../../../utils/post.utils';
@@ -25,6 +28,23 @@ export const RefCard = (props: {
 
   const urlTruncated = truncate(props.url, 50);
 
+  const [thumbnail, setThumbnail] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    const fetchThumbnail = async () => {
+      try {
+        const thumbnailUrl = await getThumbnail(props.url);
+        setThumbnail(thumbnailUrl);
+      } catch (error) {
+        console.error('Error fetching thumbnail:', error);
+      }
+    };
+
+    fetchThumbnail().catch((error) =>
+      console.error('Error in fetchThumbnail:', error)
+    );
+  }, [props.url]);
+
   return (
     <Box align="start" pad={{}}>
       <Box
@@ -46,33 +66,81 @@ export const RefCard = (props: {
           </Text>
         )}
       </Box>
-
-      <AppHeading level={4} color="#374151" style={{ fontWeight: '500' }}>
-        {titleTruncated}
-      </AppHeading>
-
-      <Paragraph
-        margin={{ vertical: '4px' }}
-        size="medium"
-        style={{ lineHeight: '18px', color: constants.colors.textLight2 }}
-        maxLines={2}>
-        {props.description}
-      </Paragraph>
-
-      <Box style={{ overflow: 'hidden' }}>
-        <Anchor
-          id={REF_URL_ANCHOR_ID}
-          style={{
-            fontSize: '16px',
-            color: '#337FBD',
-            fontWeight: '400',
-            lineBreak: 'anywhere',
-            textDecoration: 'none',
-          }}
-          target="_blank">
-          {urlTruncated}
-        </Anchor>
+      <Box
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          gap: '12px',
+          alignSelf: 'stretch',
+          flexDirection: 'row',
+        }}>
+        {thumbnail && (
+          <Box
+            style={{
+              borderRadius: '8px',
+              border: '1px solid var(--Neutral-300, #D1D5DB)',
+              background: thumbnail
+                ? `url(${thumbnail}) lightgray 50% / cover no-repeat`
+                : undefined,
+              width: '76px',
+              height: '76px',
+              flexShrink: 0,
+            }}></Box>
+        )}
+        <Box>
+          <AppHeading level={4} color="#374151" style={{ fontWeight: '500' }}>
+            {titleTruncated}
+          </AppHeading>
+          <Box style={{ overflow: 'hidden' }}>
+            <Anchor
+              id={REF_URL_ANCHOR_ID}
+              style={{
+                fontSize: '16px',
+                color: '#337FBD',
+                fontWeight: '400',
+                lineBreak: 'anywhere',
+                textDecoration: 'none',
+              }}
+              target="_blank">
+              {urlTruncated}
+            </Anchor>
+          </Box>
+        </Box>
       </Box>
     </Box>
   );
 };
+
+async function getThumbnail(url: string): Promise<string | undefined> {
+  if (!IFRAMELY_API_KEY || !IFRAMELY_API_URL) {
+    console.error('Iframely API key not found.');
+    return undefined;
+  }
+  const apiKey = IFRAMELY_API_KEY;
+  const apiUrl = `${IFRAMELY_API_URL}/iframely?url=${encodeURIComponent(url)}&api_key=${apiKey}`;
+
+  try {
+    const response = await axios.get(apiUrl);
+    const data: IframelyResponse = response.data as IframelyResponse;
+
+    // Check if 'links' and 'thumbnail' are present in the response
+    if (data.links && data.links.thumbnail && data.links.thumbnail.length > 0) {
+      // Extract the first thumbnail URL
+      const thumbnailUrl = data.links.thumbnail[0].href;
+      return thumbnailUrl;
+    } else {
+      console.error('No thumbnail found for the provided URL.');
+      return undefined;
+    }
+  } catch (error) {
+    console.error('Error fetching data from Iframely API:', error);
+    return undefined;
+  }
+}
+interface IframelyResponse {
+  links: {
+    thumbnail: {
+      href: string;
+    }[];
+  };
+}
