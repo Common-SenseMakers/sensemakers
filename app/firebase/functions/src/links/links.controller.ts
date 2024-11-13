66e24479b8c975a3b715ea8f4e6202690df7788a
Wesley Finck <wesleyfinck@gmail.com>
2024-11-12 17:32:51 -0800

    wip: include aggregated ref labels in ref meta

--- app/firebase/functions/src/links/links.controller.ts
+++ app/firebase/functions/src/links/links.controller.ts
@@ -1,7 +1,7 @@
 import { RequestHandler } from 'express';
 import { object, string } from 'yup';
 
-import { OEmbed } from '../@shared/types/types.parser';
+import { OEmbed, RefMeta } from '../@shared/types/types.parser';
 import { PostsQueryDefined } from '../@shared/types/types.posts';
 import { getServices } from '../controllers.utils';
 import { logger } from '../instances/logger';
@@ -48,10 +48,11 @@ export const getRefMetaController: RequestHandler = async (
     const refPost = postsFull[0];
     const refMeta =
       refPost.originalParsed?.support?.refs_meta?.[queryParams.ref];
-    const refData: OEmbed = {
+    const refData: RefMeta = {
       url: queryParams.ref,
       title: refMeta?.title,
       summary: refMeta?.summary,
+      refLabels: refPost.meta?.refLabels[queryParams.ref],
     };
 
     if (DEBUG) logger.debug(`${request.path}: refData`, { refData });
