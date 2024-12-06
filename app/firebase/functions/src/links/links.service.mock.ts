977e081940e4a17046c092c6edddba887389b359
Wesley Finck <wesleyfinck@gmail.com>
2024-12-06 10:06:50 -0800

    Revert "more mocking of the link service to see if its being used in deployment"
    This reverts commit 96db6d38972123ec6e619e50f152cbdd89b4cfd2.

--- app/firebase/functions/src/links/links.service.mock.ts
+++ app/firebase/functions/src/links/links.service.mock.ts
@@ -20,7 +20,7 @@ export const getLinksMock = (
   if (type.enable) {
     when(mocked.fetchOEmbed(anything())).thenCall((url: string) => {
       const oembed: OEmbed = {
-        url: url + '/mock',
+        url,
         type: 'mocked',
       };
       return { success: true, oembed };
