4cd2cac6d96f6f2cf04308dcd2aa55abcc75299c
Pepo <pepo.ospina@gmail.com>
2025-01-13 16:02:31 +0100

    not compile errors tests

--- app/firebase/functions/scripts/016-add-account-data.ts
+++ app/firebase/functions/scripts/016-add-account-data.ts
@@ -11,37 +11,17 @@ import axios from 'axios';
 import dotenv from 'dotenv';
 import fs from 'fs';
 
+import { callAddProfiles } from './scripts.utils';
+
 dotenv.config({ path: './scripts/.script.env' });
 
-const FUNCTION_URL = process.env.FUNCTIONS_URL;
-const ADMIN_API_KEY = process.env.ADMIN_API_KEY;
+const FUNCTION_URL = process.env.FUNCTIONS_URL as string;
+const ADMIN_API_KEY = process.env.ADMIN_API_KEY as string;
 const ACCOUNTS_FILE_PATH = process.env.ACCOUNTS_FILE_PATH;
 
 if (!ADMIN_API_KEY) {
   console.error('ADMIN_API_KEY environment variable is not set');
   process.exit(1);
-}
-
-async function addAccountData(payload: string[]) {
-  try {
-    if (!FUNCTION_URL) {
-      console.error('FUNCTION_URL environment variable is not set');
-      process.exit(1);
-    }
-    const response = await axios.post(
-      FUNCTION_URL + '/admin/addProfiles',
-      payload,
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          'admin-api-key': ADMIN_API_KEY,
-        },
-      }
-    );
-    return response.data;
-  } catch (error: any) {
-    console.error(error.response?.data || error.message);
-  }
 }
 
 async function processUserDataPayloads() {
@@ -51,9 +31,9 @@ async function processUserDataPayloads() {
       process.exit(1);
     }
     const rawData = fs.readFileSync(ACCOUNTS_FILE_PATH, 'utf-8');
-    const payloads: string[] = JSON.parse(rawData);
+    const profiles: string[] = JSON.parse(rawData);
 
-    await addAccountData(payloads);
+    await callAddProfiles(profiles, FUNCTION_URL, ADMIN_API_KEY);
 
     console.log('Finished processing all user data payloads');
   } catch (error: any) {
--- app/firebase/functions/scripts/scripts.utils.ts
+++ app/firebase/functions/scripts/scripts.utils.ts
@@ -16,3 +16,32 @@ export const initApp = (config: AppOptions, name: string) => {
 
   return app;
 };
+
+export async function callAddProfiles(
+  accounts: string[],
+  functionsUrl: string,
+  adminApiKey: string
+) {
+  try {
+    const response = await fetch(`${functionsUrl}/admin/addProfiles`, {
+      headers: [
+        ['Content-Type', 'application/json'],
+        ['admin-api-key', adminApiKey],
+      ],
+      method: 'post',
+      body: JSON.stringify(accounts),
+    });
+
+    if (!response.ok) {
+      const body = await response.json();
+      logger.error(`Error getting Orcid token ${JSON.stringify(body)}`);
+      throw new Error(`Error getting Orcid token: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    return data;
+  } catch (error: any) {
+    console.error(error.response?.data || error.message);
+  }
+}
