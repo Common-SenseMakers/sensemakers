cf81586f9bef29ac390335873984a0eab4d47178
Wesley Finck <wesleyfinck@gmail.com>
2025-01-20 15:21:12 +0800

    reformat merging test file and include tests for twitter

--- app/firebase/functions/test/__tests__/022-test.data.ts
+++ app/firebase/functions/test/__tests__/022-test.data.ts
@@ -1,7 +1,8 @@
 import { BlueskyThread } from '../../src/@shared/types/types.bluesky';
 import { PlatformPostPosted } from '../../src/@shared/types/types.platform.posts';
+import { TwitterThread } from '../../src/@shared/types/types.twitter';
 
-export const rootThread: PlatformPostPosted<BlueskyThread> = {
+export const rootBlueskyThread: PlatformPostPosted<BlueskyThread> = {
   post_id:
     'at://did:plc:6z5botgrc5vekq7j26xnvawq/app.bsky.feed.post/3l4wd2aares2z',
   user_id: 'did:plc:6z5botgrc5vekq7j26xnvawq',
@@ -125,7 +126,7 @@ export const rootThread: PlatformPostPosted<BlueskyThread> = {
   },
 };
 
-export const brokenThreadWithRoot: PlatformPostPosted<BlueskyThread> = {
+export const brokenBlueskyThreadWithRoot: PlatformPostPosted<BlueskyThread> = {
   post_id:
     'at://did:plc:6z5botgrc5vekq7j26xnvawq/app.bsky.feed.post/3l4wd2aares2z',
   user_id: 'did:plc:6z5botgrc5vekq7j26xnvawq',
@@ -279,159 +280,160 @@ export const brokenThreadWithRoot: PlatformPostPosted<BlueskyThread> = {
   },
 };
 
-export const brokenThreadWithoutRoot: PlatformPostPosted<BlueskyThread> = {
-  post_id: 'at://did:plc:tg3sdfq345234fdft/app.bsky.feed.post/123fasdf45',
-  user_id: 'did:plc:6z5botgrc5vekq7j26xnvawq',
-  timestampMs: 1727205208000,
-  post: {
-    thread_id: 'at://did:plc:tg3sdfq345234fdft/app.bsky.feed.post/123fasdf45',
-    posts: [
-      {
-        uri: 'at://did:plc:6z5botgrc5vekq7j26xnvawq/app.bsky.feed.post/3l4wdaif3va2h',
-        cid: 'bafyreifxy4hvz6id34zajalsc23s33s73gw3eyxbc7kwjgbi6yvcpmthoy',
-        author: {
-          did: 'did:plc:6z5botgrc5vekq7j26xnvawq',
-          handle: 'weswalla.bsky.social',
-          displayName: 'Wesley Finck',
-          avatar:
-            'https://cdn.bsky.app/img/avatar/plain/did:plc:6z5botgrc5vekq7j26xnvawq/bafkreig3weniwt64x5rmau77bgqmpo26p6qy4tusdzfccreun44nvuaczq@jpeg',
-          associated: { chat: { allowIncoming: 'following' } },
-          viewer: { muted: false, blockedBy: false },
-          labels: [],
-          createdAt: '2024-02-08T00:14:23.947Z',
-        },
-        record: {
-          $type: 'app.bsky.feed.post',
-          createdAt: '2024-09-24T19:13:28.290Z',
-          embed: {
-            $type: 'app.bsky.embed.external',
-            external: {
-              description:
-                'Semantic Scholar profile for Richard M. Ryan, with 27513 highly influential citations and 499 scientific research papers.',
-              thumb: {
-                $type: 'blob',
-                ref: {
-                  $link:
-                    'bafkreifluh6qlba467xstgk5lc5uykjszcnzpidkikctaedsiwkqqwgsym',
+export const brokenBlueskyThreadWithoutRoot: PlatformPostPosted<BlueskyThread> =
+  {
+    post_id: 'at://did:plc:tg3sdfq345234fdft/app.bsky.feed.post/123fasdf45',
+    user_id: 'did:plc:6z5botgrc5vekq7j26xnvawq',
+    timestampMs: 1727205208000,
+    post: {
+      thread_id: 'at://did:plc:tg3sdfq345234fdft/app.bsky.feed.post/123fasdf45',
+      posts: [
+        {
+          uri: 'at://did:plc:6z5botgrc5vekq7j26xnvawq/app.bsky.feed.post/3l4wdaif3va2h',
+          cid: 'bafyreifxy4hvz6id34zajalsc23s33s73gw3eyxbc7kwjgbi6yvcpmthoy',
+          author: {
+            did: 'did:plc:6z5botgrc5vekq7j26xnvawq',
+            handle: 'weswalla.bsky.social',
+            displayName: 'Wesley Finck',
+            avatar:
+              'https://cdn.bsky.app/img/avatar/plain/did:plc:6z5botgrc5vekq7j26xnvawq/bafkreig3weniwt64x5rmau77bgqmpo26p6qy4tusdzfccreun44nvuaczq@jpeg',
+            associated: { chat: { allowIncoming: 'following' } },
+            viewer: { muted: false, blockedBy: false },
+            labels: [],
+            createdAt: '2024-02-08T00:14:23.947Z',
+          },
+          record: {
+            $type: 'app.bsky.feed.post',
+            createdAt: '2024-09-24T19:13:28.290Z',
+            embed: {
+              $type: 'app.bsky.embed.external',
+              external: {
+                description:
+                  'Semantic Scholar profile for Richard M. Ryan, with 27513 highly influential citations and 499 scientific research papers.',
+                thumb: {
+                  $type: 'blob',
+                  ref: {
+                    $link:
+                      'bafkreifluh6qlba467xstgk5lc5uykjszcnzpidkikctaedsiwkqqwgsym',
+                  },
+                  mimeType: 'image/jpeg',
+                  size: 335354,
                 },
-                mimeType: 'image/jpeg',
-                size: 335354,
+                title: 'Richard M. Ryan | Semantic Scholar',
+                uri: 'https://www.semanticscholar.org/author/Richard-M.-Ryan/2242812951',
               },
-              title: 'Richard M. Ryan | Semantic Scholar',
-              uri: 'https://www.semanticscholar.org/author/Richard-M.-Ryan/2242812951',
             },
-          },
-          facets: [
-            {
-              features: [
-                {
-                  $type: 'app.bsky.richtext.facet#link',
-                  uri: 'https://www.semanticscholar.org/author/Richard-M.-Ryan/2242812951',
-                },
-              ],
-              index: { byteEnd: 118, byteStart: 79 },
-            },
-          ],
-          langs: ['en'],
-          reply: {
-            parent: {
-              cid: 'bafyreiesiupjfvjgezjj2mnyh7bbdzztqkn7gu2bgpgpipz7d5h7j7s7ym',
-              uri: 'at://did:plc:6z5botgrc5vekq7j26xnvawq/app.bsky.feed.post/3l4wd52krts24',
-            },
-            root: {
-              cid: 'bafyreig7foqn2iwo2mql5f5on4sglow6vwzlvfxq4ajtm2mlpnrmpzfrhe',
-              uri: 'at://did:plc:6z5botgrc5vekq7j26xnvawq/app.bsky.feed.post/3l4wd2aares2z',
-            },
-          },
-          text: 'one of the creators of the theory is one of the most cited academics I know of www.semanticscholar.org/author/Richa...',
-        },
-        replyCount: 1,
-        repostCount: 0,
-        likeCount: 0,
-        quoteCount: 0,
-        indexedAt: '2024-09-24T19:13:28.290Z',
-        viewer: { threadMuted: false, embeddingDisabled: false },
-        labels: [],
-      },
-      {
-        uri: 'at://did:plc:6z5botgrc5vekq7j26xnvawq/app.bsky.feed.post/3l4wdcb3w322z',
-        cid: 'bafyreicw53trheykcocuhdwvhbdkejsb6pmaiwcrzimruv32n2jericleu',
-        author: {
-          did: 'did:plc:6z5botgrc5vekq7j26xnvawq',
-          handle: 'weswalla.bsky.social',
-          displayName: 'Wesley Finck',
-          avatar:
-            'https://cdn.bsky.app/img/avatar/plain/did:plc:6z5botgrc5vekq7j26xnvawq/bafkreig3weniwt64x5rmau77bgqmpo26p6qy4tusdzfccreun44nvuaczq@jpeg',
-          associated: { chat: { allowIncoming: 'following' } },
-          viewer: { muted: false, blockedBy: false },
-          labels: [],
-          createdAt: '2024-02-08T00:14:23.947Z',
-        },
-        record: {
-          $type: 'app.bsky.feed.post',
-          createdAt: '2024-09-24T19:14:27.758Z',
-          embed: {
-            $type: 'app.bsky.embed.external',
-            external: {
-              description: 'YouTube video by Happiness & Its Causes',
-              thumb: {
-                $type: 'blob',
-                ref: {
-                  $link:
-                    'bafkreicicwvgjhubi7qhtjkfpc552bd5prs5e7oabud4ppx5ls243fhkdq',
-                },
-                mimeType: 'image/jpeg',
-                size: 209528,
+            facets: [
+              {
+                features: [
+                  {
+                    $type: 'app.bsky.richtext.facet#link',
+                    uri: 'https://www.semanticscholar.org/author/Richard-M.-Ryan/2242812951',
+                  },
+                ],
+                index: { byteEnd: 118, byteStart: 79 },
               },
-              title:
-                'MOTIVATION & LIFESTYLE with Richard Ryan at Happiness & Its Causes 2017',
-              uri: 'https://www.youtube.com/watch?v=wzN10--mNw8&t=175s',
+            ],
+            langs: ['en'],
+            reply: {
+              parent: {
+                cid: 'bafyreiesiupjfvjgezjj2mnyh7bbdzztqkn7gu2bgpgpipz7d5h7j7s7ym',
+                uri: 'at://did:plc:6z5botgrc5vekq7j26xnvawq/app.bsky.feed.post/3l4wd52krts24',
+              },
+              root: {
+                cid: 'bafyreig7foqn2iwo2mql5f5on4sglow6vwzlvfxq4ajtm2mlpnrmpzfrhe',
+                uri: 'at://did:plc:6z5botgrc5vekq7j26xnvawq/app.bsky.feed.post/3l4wd2aares2z',
+              },
             },
+            text: 'one of the creators of the theory is one of the most cited academics I know of www.semanticscholar.org/author/Richa...',
           },
-          facets: [
-            {
-              features: [
-                {
-                  $type: 'app.bsky.richtext.facet#link',
-                  uri: 'https://www.youtube.com/watch?v=wzN10--mNw8&t=175s',
-                },
-              ],
-              index: { byteEnd: 69, byteStart: 38 },
-            },
-          ],
-          langs: ['en'],
-          reply: {
-            parent: {
-              cid: 'bafyreifxy4hvz6id34zajalsc23s33s73gw3eyxbc7kwjgbi6yvcpmthoy',
-              uri: 'at://did:plc:6z5botgrc5vekq7j26xnvawq/app.bsky.feed.post/3l4wdaif3va2h',
-            },
-            root: {
-              cid: 'bafyreig7foqn2iwo2mql5f5on4sglow6vwzlvfxq4ajtm2mlpnrmpzfrhe',
-              uri: 'at://did:plc:6z5botgrc5vekq7j26xnvawq/app.bsky.feed.post/3l4wd2aares2z',
-            },
-          },
-          text: "Here's a short overview of the theory www.youtube.com/watch?v=wzN1...",
+          replyCount: 1,
+          repostCount: 0,
+          likeCount: 0,
+          quoteCount: 0,
+          indexedAt: '2024-09-24T19:13:28.290Z',
+          viewer: { threadMuted: false, embeddingDisabled: false },
+          labels: [],
         },
-        replyCount: 0,
-        repostCount: 0,
-        likeCount: 0,
-        quoteCount: 0,
-        indexedAt: '2024-09-24T19:14:27.758Z',
-        viewer: { threadMuted: false, embeddingDisabled: false },
-        labels: [],
+        {
+          uri: 'at://did:plc:6z5botgrc5vekq7j26xnvawq/app.bsky.feed.post/3l4wdcb3w322z',
+          cid: 'bafyreicw53trheykcocuhdwvhbdkejsb6pmaiwcrzimruv32n2jericleu',
+          author: {
+            did: 'did:plc:6z5botgrc5vekq7j26xnvawq',
+            handle: 'weswalla.bsky.social',
+            displayName: 'Wesley Finck',
+            avatar:
+              'https://cdn.bsky.app/img/avatar/plain/did:plc:6z5botgrc5vekq7j26xnvawq/bafkreig3weniwt64x5rmau77bgqmpo26p6qy4tusdzfccreun44nvuaczq@jpeg',
+            associated: { chat: { allowIncoming: 'following' } },
+            viewer: { muted: false, blockedBy: false },
+            labels: [],
+            createdAt: '2024-02-08T00:14:23.947Z',
+          },
+          record: {
+            $type: 'app.bsky.feed.post',
+            createdAt: '2024-09-24T19:14:27.758Z',
+            embed: {
+              $type: 'app.bsky.embed.external',
+              external: {
+                description: 'YouTube video by Happiness & Its Causes',
+                thumb: {
+                  $type: 'blob',
+                  ref: {
+                    $link:
+                      'bafkreicicwvgjhubi7qhtjkfpc552bd5prs5e7oabud4ppx5ls243fhkdq',
+                  },
+                  mimeType: 'image/jpeg',
+                  size: 209528,
+                },
+                title:
+                  'MOTIVATION & LIFESTYLE with Richard Ryan at Happiness & Its Causes 2017',
+                uri: 'https://www.youtube.com/watch?v=wzN10--mNw8&t=175s',
+              },
+            },
+            facets: [
+              {
+                features: [
+                  {
+                    $type: 'app.bsky.richtext.facet#link',
+                    uri: 'https://www.youtube.com/watch?v=wzN10--mNw8&t=175s',
+                  },
+                ],
+                index: { byteEnd: 69, byteStart: 38 },
+              },
+            ],
+            langs: ['en'],
+            reply: {
+              parent: {
+                cid: 'bafyreifxy4hvz6id34zajalsc23s33s73gw3eyxbc7kwjgbi6yvcpmthoy',
+                uri: 'at://did:plc:6z5botgrc5vekq7j26xnvawq/app.bsky.feed.post/3l4wdaif3va2h',
+              },
+              root: {
+                cid: 'bafyreig7foqn2iwo2mql5f5on4sglow6vwzlvfxq4ajtm2mlpnrmpzfrhe',
+                uri: 'at://did:plc:6z5botgrc5vekq7j26xnvawq/app.bsky.feed.post/3l4wd2aares2z',
+              },
+            },
+            text: "Here's a short overview of the theory www.youtube.com/watch?v=wzN1...",
+          },
+          replyCount: 0,
+          repostCount: 0,
+          likeCount: 0,
+          quoteCount: 0,
+          indexedAt: '2024-09-24T19:14:27.758Z',
+          viewer: { threadMuted: false, embeddingDisabled: false },
+          labels: [],
+        },
+      ],
+      author: {
+        id: 'did:plc:6z5botgrc5vekq7j26xnvawq',
+        username: 'weswalla.bsky.social',
+        displayName: 'Wesley Finck',
+        avatar:
+          'https://cdn.bsky.app/img/avatar/plain/did:plc:6z5botgrc5vekq7j26xnvawq/bafkreig3weniwt64x5rmau77bgqmpo26p6qy4tusdzfccreun44nvuaczq@jpeg',
       },
-    ],
-    author: {
-      id: 'did:plc:6z5botgrc5vekq7j26xnvawq',
-      username: 'weswalla.bsky.social',
-      displayName: 'Wesley Finck',
-      avatar:
-        'https://cdn.bsky.app/img/avatar/plain/did:plc:6z5botgrc5vekq7j26xnvawq/bafkreig3weniwt64x5rmau77bgqmpo26p6qy4tusdzfccreun44nvuaczq@jpeg',
     },
-  },
-};
+  };
 
-export const brokenThreadWithRootNotPartOfMain: PlatformPostPosted<BlueskyThread> =
+export const brokenBlueskyThreadWithRootNotPartOfMain: PlatformPostPosted<BlueskyThread> =
   {
     post_id:
       'at://did:plc:6z5botgrc5vekq7j26xnvawq/app.bsky.feed.post/3l4wd2aares2z',
@@ -582,6 +584,178 @@ export const brokenThreadWithRootNotPartOfMain: PlatformPostPosted<BlueskyThread
         displayName: 'Wesley Finck',
         avatar:
           'https://cdn.bsky.app/img/avatar/plain/did:plc:6z5botgrc5vekq7j26xnvawq/bafkreig3weniwt64x5rmau77bgqmpo26p6qy4tusdzfccreun44nvuaczq@jpeg',
+      },
+    },
+  };
+
+export const rootTwitterThread: PlatformPostPosted<TwitterThread> = {
+  post_id: '1234',
+  user_id: '12345678',
+  timestampMs: 1727204998385,
+  post: {
+    conversation_id: '1234',
+    tweets: [
+      {
+        text: 'this is a thread about something.',
+        id: '1234',
+        created_at: '2025-01-09T05:01:52.000Z',
+        conversation_id: '1234',
+        edit_history_tweet_ids: ['1877219467173761329'],
+        author_id: '12345678',
+      },
+      {
+        referenced_tweets: [
+          {
+            type: 'replied_to',
+            id: '1234',
+          },
+        ],
+        text: 'A Luddite is not anti-progress. They just have a different conception of what progress means',
+        id: '1235',
+        created_at: '2025-01-09T05:02:52.000Z',
+        conversation_id: '1234',
+        edit_history_tweet_ids: ['1877219467173761329'],
+        author_id: '12345678',
+      },
+    ],
+    author: {
+      id: '12345678',
+      username: 'wesleyfinck',
+      name: 'Wesley Finck',
+    },
+  },
+};
+
+export const brokenTwitterThreadWithRoot: PlatformPostPosted<TwitterThread> = {
+  post_id: '1234',
+  user_id: '12345678',
+  timestampMs: 1727204998390,
+  post: {
+    conversation_id: '1234',
+    tweets: [
+      {
+        text: 'this is the 3rd post in a thread',
+        id: '1236',
+        created_at: '2025-01-09T05:03:52.000Z',
+        conversation_id: '1234',
+        edit_history_tweet_ids: ['1877219467173761329'],
+        author_id: '12345678',
+        referenced_tweets: [
+          {
+            type: 'replied_to',
+            id: '1235',
+          },
+        ],
+      },
+      {
+        text: 'this is the 4rd post in a thread',
+        id: '1237',
+        created_at: '2025-01-09T05:04:52.000Z',
+        conversation_id: '1234',
+        edit_history_tweet_ids: ['1877219467173761329'],
+        author_id: '12345678',
+        referenced_tweets: [
+          {
+            type: 'replied_to',
+            id: '1236',
+          },
+        ],
+      },
+    ],
+    author: {
+      id: '12345678',
+      username: 'wesleyfinck',
+      name: 'Wesley Finck',
+    },
+  },
+};
+
+export const brokenTwitterThreadWithoutRoot: PlatformPostPosted<TwitterThread> =
+  {
+    post_id: '5678',
+    user_id: '12345678',
+    timestampMs: 1727204998390,
+    post: {
+      conversation_id: '5678',
+      tweets: [
+        {
+          text: 'this is the 3rd post in a thread',
+          id: '5679',
+          created_at: '2025-01-09T05:03:52.000Z',
+          conversation_id: '1234',
+          edit_history_tweet_ids: ['1877219467173761329'],
+          author_id: '12345678',
+          referenced_tweets: [
+            {
+              type: 'replied_to',
+              id: '5677',
+            },
+          ],
+        },
+        {
+          text: 'this is the 4rd post in a thread',
+          id: '5680',
+          created_at: '2025-01-09T05:04:52.000Z',
+          conversation_id: '1234',
+          edit_history_tweet_ids: ['1877219467173761329'],
+          author_id: '12345678',
+          referenced_tweets: [
+            {
+              type: 'replied_to',
+              id: '5679',
+            },
+          ],
+        },
+      ],
+      author: {
+        id: '12345678',
+        username: 'wesleyfinck',
+        name: 'Wesley Finck',
+      },
+    },
+  };
+
+export const brokenTwitterThreadWithRootNotPartOfMain: PlatformPostPosted<TwitterThread> =
+  {
+    post_id: '1234',
+    user_id: '12345678',
+    timestampMs: 1727204998390,
+    post: {
+      conversation_id: '1234',
+      tweets: [
+        {
+          text: 'this is the 3rd post in a thread',
+          id: '1236',
+          created_at: '2025-01-09T05:03:52.000Z',
+          conversation_id: '1234',
+          edit_history_tweet_ids: ['1877219467173761329'],
+          author_id: '12345678',
+          referenced_tweets: [
+            {
+              type: 'replied_to',
+              id: '1234',
+            },
+          ],
+        },
+        {
+          text: 'this is the 4rd post in a thread',
+          id: '1237',
+          created_at: '2025-01-09T05:04:52.000Z',
+          conversation_id: '1234',
+          edit_history_tweet_ids: ['1877219467173761329'],
+          author_id: '12345678',
+          referenced_tweets: [
+            {
+              type: 'replied_to',
+              id: '1236',
+            },
+          ],
+        },
+      ],
+      author: {
+        id: '12345678',
+        username: 'wesleyfinck',
+        name: 'Wesley Finck',
       },
     },
   };
