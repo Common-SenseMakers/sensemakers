81c396391b65fbab6366fb07fbe3945ae77f703f
Wesley Finck <wesleyfinck@gmail.com>
2025-01-30 23:29:29 +0800

    implement the rest of custom posthog events

--- app/webapp/src/profiles/AccountProfileAnchor.tsx
+++ app/webapp/src/profiles/AccountProfileAnchor.tsx
@@ -1,6 +1,8 @@
 import { Anchor } from 'grommet';
+import posthog from 'posthog-js';
 import React from 'react';
 
+import { POSTHOG_EVENTS } from '../analytics/posthog.events';
 import { getAccountDetails } from '../post/platform-specific.details';
 import { AccountProfileRead } from '../shared/types/types.profiles';
 import { useThemeContext } from '../ui-components/ThemedApp';
@@ -17,6 +19,12 @@ export const AccountProfileAnchor = (props: {
 
   return (
     <Anchor
+      onClick={() => {
+        posthog?.capture(POSTHOG_EVENTS.CLICKED_PROFILE_URL, {
+          url: url,
+          username: account.profile?.username,
+        });
+      }}
       style={{
         fontSize: '14px',
         fontStyle: 'normal',
