2b787a1ca4a6d7c408098d3724c870255843c67d
Wesley Finck <wesleyfinck@gmail.com>
2025-02-04 22:53:07 +0800

    property name fix for mastodon connect error text

--- app/webapp/src/user-login/contexts/platforms/MastodonContext.tsx
+++ app/webapp/src/user-login/contexts/platforms/MastodonContext.tsx
@@ -136,7 +136,7 @@ export const MastodonContext = (props: PropsWithChildren) => {
         }
       } catch (err) {
         log('Error connecting to Mastodon', err);
-        setError(t(IntroKeys.errorConnectMastodon, { domain }));
+        setError(t(IntroKeys.errorConnectMastodon, { mastodonServer: domain }));
         setLoginFlowState(LoginFlowState.Idle);
         setPlatformConnectedStatus(
           PLATFORM.Mastodon,
