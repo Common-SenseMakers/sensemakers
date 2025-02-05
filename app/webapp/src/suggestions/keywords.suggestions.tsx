047e892480af4cb467149a9077bf502d7e2efe7d
Pepo <pepo.ospina@gmail.com>
2025-02-05 15:55:19 +0100

    keywords suggestions per cluster

--- app/webapp/src/suggestions/keywords.suggestions.tsx
+++ app/webapp/src/suggestions/keywords.suggestions.tsx
@@ -1,7 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
 import { Box } from 'grommet';
+import { useParams } from 'react-router-dom';
 
 import { useAppFetch } from '../api/app.fetch';
+import { ALL_CLUSTER_NAME } from '../posts.fetcher/cluster.context';
 import { KEYWORDS_COLORS } from '../semantics/patterns/keywords/Keywords.component';
 import { GetIndexedEntries } from '../shared/types/types.posts';
 import { AppLabelsEditor } from '../ui-components/AppLabelsEditor';
@@ -11,12 +13,16 @@ export const KeywordsSuggestions = (props: {
 }) => {
   const appFetch = useAppFetch();
 
+  const { clusterId } = useParams();
+  const clusterSelected =
+    clusterId !== ALL_CLUSTER_NAME ? clusterId : undefined;
+
   const { data: keywords } = useQuery({
-    queryKey: ['keywords-suggestions'],
+    queryKey: ['keywords-suggestions', clusterSelected],
     queryFn: async () => {
       try {
         const payload: GetIndexedEntries = {
-          clusterId: undefined,
+          clusterId: clusterSelected,
           afterId: undefined,
         };
         const result = await appFetch<string[], GetIndexedEntries>(
